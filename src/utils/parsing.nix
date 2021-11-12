3ec6baf2c46680fdb6ef6a5a4ffa6386612144a8
DavHau <hsngrmpf+github@gmail.com>
2021-11-12 21:52:32 +0700

    support parsing `github:...` URLs

--- src/utils/parsing.nix
+++ src/utils/parsing.nix
@@ -7,26 +7,41 @@ let
   b = builtins;
 
   identifyGitUrl = url:
-    lib.hasPrefix "git+" url;
+    lib.hasPrefix "git+" url
+    || b.match ''^github:.*/.*#.*'' url != null;
 
   parseGitUrl = url:
     let
-      splitUrlRev = lib.splitString "#" url;
-      rev = lib.last splitUrlRev;
-      urlOnly = lib.head splitUrlRev;
+      githubMatch = b.match ''^github:(.*)/(.*)#(.*)$'' url;
     in
-      if lib.hasPrefix "git+ssh://" urlOnly then
-        {
-          inherit rev;
-          url = "https://${(lib.last (lib.splitString "@" url))}";
-        }
-      else if lib.hasPrefix "git+https://" urlOnly then
-        {
-          inherit rev;
-          url = lib.removePrefix "git+" urlOnly;
-        }
+      if githubMatch != null then
+        let
+          owner = b.elemAt githubMatch 0;
+          repo = b.elemAt githubMatch 1;
+          rev = b.elemAt githubMatch 2;
+        in
+          {
+            url = "https://github.com/${owner}/${repo}";
+            inherit rev;
+          }
       else
-        throw "Cannot parse git url: ${url}";
+        let
+          splitUrlRev = lib.splitString "#" url;
+          rev = lib.last splitUrlRev;
+          urlOnly = lib.head splitUrlRev;
+        in
+          if lib.hasPrefix "git+ssh://" urlOnly then
+            {
+              inherit rev;
+              url = "https://${(lib.last (lib.splitString "@" url))}";
+            }
+          else if lib.hasPrefix "git+https://" urlOnly then
+            {
+              inherit rev;
+              url = lib.removePrefix "git+" urlOnly;
+            }
+          else
+            throw "Cannot parse git url: ${url}";
 
 
 in
