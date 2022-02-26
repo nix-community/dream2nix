838db1097e3893482c618b9a6197542f4b5c5848
DavHau <hsngrmpf+github@gmail.com>
2022-02-26 10:59:06 +0700

    make V1 translators V2 comaptible

--- src/translators/default.nix
+++ src/translators/default.nix
@@ -19,47 +19,80 @@ let
 
   l = lib // builtins;
 
-  translators =
-    dlib.translators.mapTranslators
-      (translatorModule:
-        let
-          translator =
-            translatorModule
+  # transforms all V1 translators to V2 translators
+  ensureTranslatorV2 = translator:
+    let
+      version = translator.version or 1;
+      cleanedArgs = args: l.removeAttrs args [ "projets" "tree" ];
 
-            # for pure translators
-            #   - import the `translate` function
-            #   - generate `translateBin`
-            // (lib.optionalAttrs (translatorModule ? translate) {
-              translate = callPackageDream translatorModule.translate {
+      upgradedTranslator =
+        translator // {
+          translate = args:
+            l.map
+              (proj: proj // {
+                dreamLock = translator.translate
+                  ((cleanedArgs args) // {
+                    source = "${args.source}${proj.relPath}";
+                  });
+              })
+              args.projects;
+        };
+    in
+      if version == 2 then
+        translator
+      else
+        upgradedTranslator;
+
+
+  makeTranslatorV2 = translatorModule:
+    ensureTranslatorV2 (makeTranslator translatorModule);
+
+
+  makeTranslator =
+    translatorModule:
+      let
+        translator =
+          translatorModule
+
+          # for pure translators
+          #   - import the `translate` function
+          #   - generate `translateBin`
+          // (lib.optionalAttrs (translatorModule ? translate) {
+            translate = callPackageDream translatorModule.translate {
+              translatorName = translatorModule.name;
+            };
+            translateBin =
+              wrapPureTranslator
+              (with translatorModule; [ subsystem type name ]);
+          })
+
+          # for impure translators:
+          #   - import the `translateBin` function
+          // (lib.optionalAttrs (translatorModule ? translateBin) {
+            translateBin = callPackageDream translatorModule.translateBin
+              {
                 translatorName = translatorModule.name;
               };
-              translateBin =
-                wrapPureTranslator
-                (with translatorModule; [ subsystem type name ]);
-            })
+          });
 
-            # for impure translators:
-            #   - import the `translateBin` function
-            // (lib.optionalAttrs (translatorModule ? translateBin) {
-              translateBin = callPackageDream translatorModule.translateBin
-                {
-                  translatorName = translatorModule.name;
-                };
-            });
+          # supply calls to translate with default arguments
+          translatorWithDefaults = translator // {
+            translate = args:
+              translator.translate
+                (
+                  (dlib.translators.getextraArgsDefaults
+                    (translator.extraArgs or {}))
+                  // args
+                );
+          };
 
-            # supply calls to translate with default arguments
-            translatorWithDefaults = translator // {
-              translate = args:
-                translator.translate
-                  (
-                    (dlib.translators.getextraArgsDefaults
-                      (translator.extraArgs or {}))
-                    // args
-                  );
-            };
+      in
+        translatorWithDefaults;
 
-        in
-          translatorWithDefaults);
+
+  translators = dlib.translators.mapTranslators makeTranslator;
+
+  translatorsV2 = dlib.translators.mapTranslators makeTranslatorV2;
 
 
   # adds a translateBin to a pure translator
@@ -102,6 +135,7 @@ in
 {
   inherit
     translators
+    translatorsV2
   ;
 
   inherit (dlib.translators)
