95c6595165dde4483e48a552930a6d29305b9fe7
DavHau <hsngrmpf+github@gmail.com>
2022-03-02 12:05:43 +0700

    yarn-lock translator: use simpleTranslate2

--- src/translators/nodejs/pure/yarn-lock/default.nix
+++ src/translators/nodejs/pure/yarn-lock/default.nix
@@ -90,18 +90,49 @@ let
       #     workspacesPackageJson."${workspace}".;
     in
 
-    utils.simpleTranslate
+    utils.simpleTranslate2
       ({
-        getDepByNameVer,
-        dependenciesByOriginalID,
+        objectsByKey,
         ...
-      }:
+      }: let
 
-      rec {
+        getSourceType = rawObj: finalObj: let
+          dObj = rawObj;
+        in
+          if
+            lib.hasInfix "@github:" dObj.yarnName
+
+            || (dObj ? resolved
+                && lib.hasInfix "codeload.github.com/" dObj.resolved)
+
+            || lib.hasInfix "@git+" dObj.yarnName
+
+            # example:
+            # "jest-image-snapshot@https://github.com/machard/jest-image-snapshot#machard-patch-1":
+            #   version "4.2.0"
+            #   resolved "https://github.com/machard/jest-image-snapshot#d087e8683859dba2964b5866a4d1eb02ba64e7b9"
+            || (lib.hasInfix "@https://github.com" dObj.yarnName
+                && lib.hasPrefix "https://github.com" dObj.resolved)
+          then
+            if dObj ? integrity
+            then
+              b.trace (
+                "Warning: Using git despite integrity exists for"
+                + "${finalObj.name}"
+              )
+                "git"
+            else "git"
+
+          else if
+            lib.hasInfix "@link:" dObj.yarnName
+            || lib.hasInfix "@file:" dObj.yarnName
+          then "path"
+
+          else "http";
+
+      in rec {
 
         inherit translatorName;
-
-        inputData = yarnLock;
 
         defaultPackage =
           if name != "{automatic}" then
@@ -112,7 +143,7 @@ let
               + "Please specify extra argument 'name'"
             ));
 
-        packages =
+        exportedPackages =
           { "${defaultPackage}" = packageJson.version or "unknown"; };
           # // (nodejsUtils.getWorkspacePackages tree workspaces);
 
@@ -120,200 +151,165 @@ let
 
         subsystemAttrs = { nodejsVersion = args.nodejs; };
 
-        mainPackageDependencies =
-          lib.mapAttrsToList
-            (depName: depSemVer:
+        keys = {
+          yarnName = rawObj: finalObj:
+            rawObj.yarnName;
+        };
+
+        extractors = {
+          name = rawObj: finalObj:
+            if lib.hasInfix "@git+" rawObj.yarnName then
+              lib.head (lib.splitString "@git+" rawObj.yarnName)
+            # Example:
+            # @matrix-org/olm@https://gitlab.matrix.org/api/v4/projects/27/packages/npm/@matrix-org/olm/-/@matrix-org/olm-3.2.3.tgz
+            else if lib.hasInfix "@https://" rawObj.yarnName then
+              lib.head (lib.splitString "@https://" rawObj.yarnName)
+            else
               let
-                depYarnKey = "${depName}@${depSemVer}";
-                dependencyAttrs =
-                  if ! inputData ? "${depYarnKey}" then
-                    throw "Cannot find entry for top level dependency: '${depYarnKey}'"
-                  else
-                    inputData."${depYarnKey}";
+                split = lib.splitString "@" rawObj.yarnName;
+                version = lib.last split;
               in
-                {
-                  name = depName;
-                  version = dependencyAttrs.version;
-                }
-            )
-            packageJsonDeps;
+                if lib.hasPrefix "@" rawObj.yarnName then
+                  lib.removeSuffix "@${version}" rawObj.yarnName
+                else
+                  lib.head split;
 
-        serializePackages = inputData:
-          lib.mapAttrsToList
-            (yarnName: depAttrs: depAttrs // { inherit yarnName; })
-            yarnLock;
+          version = rawObj: finalObj:
+            rawObj.version;
 
-        getOriginalID = dependencyObject:
-          dependencyObject.yarnName;
-
-        getName = dependencyObject:
-          if lib.hasInfix "@git+" dependencyObject.yarnName then
-            lib.head (lib.splitString "@git+" dependencyObject.yarnName)
-          # Example:
-          # @matrix-org/olm@https://gitlab.matrix.org/api/v4/projects/27/packages/npm/@matrix-org/olm/-/@matrix-org/olm-3.2.3.tgz
-          else if lib.hasInfix "@https://" dependencyObject.yarnName then
-            lib.head (lib.splitString "@https://" dependencyObject.yarnName)
-          else
-            let
-              split = lib.splitString "@" dependencyObject.yarnName;
-              version = lib.last split;
+          dependencies = rawObj: finalObj: let
+            dependencies = let
+              deps =
+                rawObj.dependencies or {}
+                // rawObj.optionalDependencies or {};
             in
-              if lib.hasPrefix "@" dependencyObject.yarnName then
-                lib.removeSuffix "@${version}" dependencyObject.yarnName
-              else
-                lib.head split;
-
-        getVersion = dependencyObject:
-          dependencyObject.version;
-
-        getDependencies = dependencyObject:
-          let
-            dependencies =
-              let
-                deps =
-                  dependencyObject.dependencies or {}
-                  // dependencyObject.optionalDependencies or {};
-              in
-                lib.mapAttrsToList
-                  (name: version: { "${name}" = version; })
-                  deps;
+              lib.mapAttrsToList
+                (name: version: { "${name}" = version; })
+                deps;
           in
             lib.forEach
               dependencies
               (dependency:
                 builtins.head (
                   lib.mapAttrsToList
-                    (name: versionSpec:
-                      let
-                        yarnName = "${name}@${versionSpec}";
-                        depObject = dependenciesByOriginalID."${yarnName}";
-                        version = depObject.version;
-                      in
-                        if ! dependenciesByOriginalID ? ${yarnName} then
-                          # handle missing lock file entry
-                          let
-                            versionMatch =
-                              b.match ''.*\^([[:digit:]|\.]+)'' versionSpec;
-                          in
-                            {
-                              inherit name;
-                              version = b.elemAt versionMatch 0;
-                            }
-                        else
-                          { inherit name version; }
+                    (name: versionSpec: let
+                      yarnName = "${name}@${versionSpec}";
+                      depObject = objectsByKey.yarnName."${yarnName}";
+                      version = depObject.version;
+                    in
+                      if ! objectsByKey.yarnName ? ${yarnName} then
+                        # handle missing lock file entry
+                        let
+                          versionMatch =
+                            b.match ''.*\^([[:digit:]|\.]+)'' versionSpec;
+                        in
+                          {
+                            inherit name;
+                            version = b.elemAt versionMatch 0;
+                          }
+                      else
+                        { inherit name version; }
                     )
                     dependency
                 )
               );
 
-        getSourceType = dependencyObject:
-          let
-            dObj = dependencyObject;
-          in
-            if
-              lib.hasInfix "@github:" dObj.yarnName
-
-                || dObj ? resolved
-                    && lib.hasInfix "codeload.github.com/" dObj.resolved
-
-                || lib.hasInfix "@git+" dObj.yarnName
-
-                # example:
-                # "jest-image-snapshot@https://github.com/machard/jest-image-snapshot#machard-patch-1":
-                #   version "4.2.0"
-                #   resolved "https://github.com/machard/jest-image-snapshot#d087e8683859dba2964b5866a4d1eb02ba64e7b9"
-                || (lib.hasInfix "@https://github.com" dObj.yarnName
-                    && lib.hasPrefix "https://github.com" dObj.resolved) then
-
-              if dObj ? integrity then
-                b.trace (
-                  "Warning: Using git despite integrity exists for"
-                  + "${getName dObj}"
-                )
-                  "git"
-              else
-                "git"
-            else if lib.hasInfix "@link:" dObj.yarnName
-                || lib.hasInfix "@file:" dObj.yarnName then
-              "path"
-            else
-              "http";
-
-
-        sourceConstructors = {
-          git = dependencyObject:
-            if utils.identifyGitUrl dependencyObject.resolved then
-              (utils.parseGitUrl dependencyObject.resolved) // {
-                version = dependencyObject.version;
-              }
-            else
-              let
-                githubUrlInfos = lib.splitString "/" dependencyObject.resolved;
-                owner = lib.elemAt githubUrlInfos 3;
-                repo = lib.elemAt githubUrlInfos 4;
-              in
-                if b.length githubUrlInfos == 7 then
-                  let
-                    rev = lib.elemAt githubUrlInfos 6;
-                  in
-                    {
-                      url = "https://github.com/${owner}/${repo}";
-                      inherit rev;
-                    }
-                else if b.length githubUrlInfos == 5 then
-                  let
-                    urlAndRev = lib.splitString "#" dependencyObject.resolved;
-                  in
-                    {
-                      url = lib.head urlAndRev;
-                      rev = lib.last urlAndRev;
-                    }
-                else
-                  throw (
-                    "Unable to parse git dependency for: "
-                    + "${getName dependencyObject}#${getVersion dependencyObject}"
-                  );
-
-
-          path = dependencyObject:
-            if lib.hasInfix "@link:" dependencyObject.yarnName then
-              {
-                path =
-                  lib.last (lib.splitString "@link:" dependencyObject.yarnName);
-              }
-            else if lib.hasInfix "@file:" dependencyObject.yarnName then
-              {
-                path =
-                  lib.last (lib.splitString "@file:" dependencyObject.yarnName);
-              }
-            else
-              throw "unknown path format ${b.toJSON dependencyObject}";
-
-          http = dependencyObject:
-            {
-              type = "http";
-              hash =
-                if dependencyObject ? integrity then
-                  dependencyObject.integrity
+            sourceSpec = rawObj: finalObj: let
+              type = getSourceType rawObj finalObj;
+            in
+              { inherit type; }
+              //
+              (if type == "git"
+              then
+                if utils.identifyGitUrl rawObj.resolved then
+                  (utils.parseGitUrl rawObj.resolved) // {
+                    version = rawObj.version;
+                  }
                 else
                   let
-                    hash =
-                      lib.last (lib.splitString "#" dependencyObject.resolved);
+                    githubUrlInfos = lib.splitString "/" rawObj.resolved;
+                    owner = lib.elemAt githubUrlInfos 3;
+                    repo = lib.elemAt githubUrlInfos 4;
                   in
-                    if lib.stringLength hash == 40 then
-                      hash
+                    if b.length githubUrlInfos == 7 then
+                      let
+                        rev = lib.elemAt githubUrlInfos 6;
+                      in
+                        {
+                          url = "https://github.com/${owner}/${repo}";
+                          inherit rev;
+                        }
+                    else if b.length githubUrlInfos == 5 then
+                      let
+                        urlAndRev = lib.splitString "#" rawObj.resolved;
+                      in
+                        {
+                          url = lib.head urlAndRev;
+                          rev = lib.last urlAndRev;
+                        }
                     else
-                      throw "Missing integrity for ${dependencyObject.yarnName}";
-              url = lib.head (lib.splitString "#" dependencyObject.resolved);
-            };
+                      throw (
+                        "Unable to parse git dependency for: "
+                        + "${finalObj.name}#${finalObj.version}"
+                      )
+
+                else if type == "path"
+                then
+                  if lib.hasInfix "@link:" rawObj.yarnName then
+                    {
+                      path =
+                        lib.last (lib.splitString "@link:" rawObj.yarnName);
+                    }
+                  else if lib.hasInfix "@file:" rawObj.yarnName then
+                    {
+                      path =
+                        lib.last (lib.splitString "@file:" rawObj.yarnName);
+                    }
+                  else
+                    throw "unknown path format ${b.toJSON rawObj}"
+
+                else  # type == "http"
+                  {
+                    type = "http";
+                    hash =
+                      if rawObj ? integrity then
+                        rawObj.integrity
+                      else
+                        let
+                          hash =
+                            lib.last (lib.splitString "#" rawObj.resolved);
+                        in
+                          if lib.stringLength hash == 40 then
+                            hash
+                          else
+                            throw "Missing integrity for ${rawObj.yarnName}";
+                    url = lib.head (lib.splitString "#" rawObj.resolved);
+                  });
         };
 
-        # TODO: implement createMissingSource
-        # createMissingSource = name: version:
-        #   let
-        #     pname = lib.last (lib.splitString "/" name);
-        #   in
+        extraDependencies =
+          l.mapAttrsToList
+            (name: semVer: let
+              depYarnKey = "${name}@${semVer}";
+              dependencyAttrs =
+                if ! yarnLock ? "${depYarnKey}" then
+                  throw "Cannot find entry for top level dependency: '${depYarnKey}'"
+                else
+                  yarnLock."${depYarnKey}";
+            in
+              {
+                name = defaultPackage;
+                version = packageJson.version;
+                dependencies = [
+                  {inherit name; version = dependencyAttrs.version;}
+                ];
+              })
+            packageJsonDeps;
 
+        serializedRawObjects =
+          lib.mapAttrsToList
+            (yarnName: depAttrs: depAttrs // { inherit yarnName; })
+            yarnLock;
 
       });
 
