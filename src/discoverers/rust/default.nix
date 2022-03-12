5b1a71c10e6e0e5d2006af85cc1ea3d42de9cf63
DavHau <hsngrmpf+github@gmail.com>
2022-03-12 17:51:43 +0700

    add discoverer for rust

---
+++ src/discoverers/rust/default.nix
@@ -0,0 +1,37 @@
+{
+  dlib,
+  lib,
+  subsystem,
+}: let
+  l = lib // builtins;
+
+  discover = {tree}: let
+    cargoToml = tree.files."Cargo.toml".tomlContent;
+
+    subdirProjects =
+      l.flatten
+      (l.mapAttrsToList
+        (dirName: dir: discover {tree = dir;})
+        (tree.directories or {}));
+  in
+    # A directory is identified as a project only if it contains a Cargo.toml
+    # and a Cargo.lock.
+    if
+      tree
+      ? files."Cargo.toml"
+      && tree ? files."Cargo.lock"
+    then
+      [
+        (dlib.construct.discoveredProject {
+          inherit subsystem;
+          relPath = tree.relPath;
+          name = cargoToml.package.name;
+          translators = ["cargo-lock"];
+          subsystemInfo = {};
+        })
+      ]
+      ++ subdirProjects
+    else subdirProjects;
+in {
+  inherit discover;
+}
