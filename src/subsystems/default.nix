624175da853ff6e0be76f9e9a9f4faee911d22c8
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-05-24 11:26:24 +0300

    fix: collect all modules instead of only collecting modules for built-in subsystems

--- src/subsystems/default.nix
+++ src/subsystems/default.nix
@@ -9,13 +9,21 @@
   builders = callPackageDream ./builders.nix {};
   translators = callPackageDream ./translators.nix {};
   discoverers = callPackageDream ./discoverers.nix {};
+
+  # maps modules
+  # ex: {rust = <translators attrset>;} -> {rust.translators = <translators attrset>;}
+  mapModules = modules: name:
+    l.mapAttrs'
+    (
+      subsystem: modules:
+        l.nameValuePair subsystem {${name} = modules;}
+    )
+    modules;
+
+  modules = [
+    (mapModules builders.builders "builders")
+    (mapModules translators.translators "translators")
+    (mapModules discoverers.discoverers "discoverers")
+  ];
 in
-  l.genAttrs
-  dlib.subsystems
-  (
-    subsystem: {
-      builders = builders.builders."${subsystem}";
-      translators = translators.translators."${subsystem}";
-      discoverers = discoverers.discoverers."${subsystem}";
-    }
-  )
+  l.foldl' l.recursiveUpdate {} modules
