8836a3f42299baeb33133317b419dfc53de4317b
DavHau <hsngrmpf+github@gmail.com>
2022-05-31 14:38:28 +0200

    WIP stack-lock translator & haskell builder

---
+++ src/subsystems/haskell/builders/default/default.nix
@@ -0,0 +1,122 @@
+{lib, ...}: let
+  l = lib // builtins;
+in {
+  type = "pure";
+
+  build = {
+    lib,
+    pkgs,
+    stdenv,
+    # dream2nix inputs
+    externals,
+    utils,
+    ...
+  }: {
+    ### FUNCTIONS
+    # AttrSet -> Bool) -> AttrSet -> [x]
+    getCyclicDependencies, # name: version: -> [ {name=; version=; } ]
+    getDependencies, # name: version: -> [ {name=; version=; } ]
+    getSource, # name: version: -> store-path
+    # to get information about the original source spec
+    getSourceSpec, # name: version: -> {type="git"; url=""; hash="";}
+    ### ATTRIBUTES
+    subsystemAttrs, # attrset
+    defaultPackageName, # string
+    defaultPackageVersion, # string
+    # all exported (top-level) package names and versions
+    # attrset of pname -> version,
+    packages,
+    # all existing package names and versions
+    # attrset of pname -> versions,
+    # where versions is a list of version strings
+    packageVersions,
+    # function which applies overrides to a package
+    # It must be applied by the builder to each individual derivation
+    # Example:
+    #   produceDerivation name (mkDerivation {...})
+    produceDerivation,
+    ...
+  } @ args: let
+    b = builtins;
+
+    all-cabal-hashes = pkgs.runCommand "all-cabal-hashes" {} ''
+      mkdir $out
+      cd $out
+      tar --strip-components 1 -xf ${pkgs.all-cabal-hashes}
+    '';
+
+    # the main package
+    defaultPackage = allPackages."${defaultPackageName}"."${defaultPackageVersion}";
+
+    # packages to export
+    packages =
+      lib.mapAttrs
+      (name: version: {
+        "${version}" = allPackages.${name}.${version};
+      })
+      args.packages;
+
+    # manage packages in attrset to prevent duplicated evaluation
+    allPackages =
+      lib.mapAttrs
+      (name: versions:
+        lib.genAttrs
+        versions
+        (version: makeOnePackage name version))
+      packageVersions;
+
+    # Generates a derivation for a specific package name + version
+    makeOnePackage = name: version: let
+      pkg = pkgs.haskell.packages.ghc8107.mkDerivation (rec {
+          pname = utils.sanitizeDerivationName name;
+          inherit version;
+          license = null;
+
+          src = getSource name version;
+
+          isLibrary = true;
+          isExecutable = true;
+          doCheck = false;
+          doBenchmark = false;
+
+          libraryToolDepends = libraryHaskellDepends;
+          executableHaskellDepends = libraryHaskellDepends;
+          testHaskellDepends = libraryHaskellDepends;
+          testToolDepends = libraryHaskellDepends;
+
+          libraryHaskellDepends =
+            (with pkgs.haskell.packages.ghc8107; [
+              # Cabal_3_2_1_0
+              hspec
+              QuickCheck
+            ])
+            ++ (
+              map
+              (dep: allPackages."${dep.name}"."${dep.version}")
+              (getDependencies name version)
+            );
+
+          # TODO: Implement build phases
+        }
+        /*
+         For all transitive dependencies, overwrite cabal file with the one
+         from all-cabal-hashes.
+         We want to ensure that the cabal file is the latest revision.
+         See: https://github.com/haskell-infra/hackage-trustees/blob/master/revisions-information.md
+         */
+        // (l.optionalAttrs (name != defaultPackageName) {
+          preConfigure = ''
+            cp ${all-cabal-hashes}/${name}/${version}/${name}.cabal ./
+          '';
+        })
+        # enable tests only for the top-level package
+        // (l.optionalAttrs (name == defaultPackageName) {
+          doCheck = true;
+        }));
+    in
+      # apply packageOverrides to current derivation
+      produceDerivation name pkg;
+  in {
+    inherit defaultPackage packages;
+  };
+}
---
+++ src/subsystems/haskell/translators/stack-lock/utils.nix
@@ -0,0 +1,76 @@
+{
+  lib,
+  dlib,
+  pkgs,
+}: let
+  l = lib // builtins;
+
+  flakeCompat = import (builtins.fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/tarball/b4a34015c698c7793d592d66adbab377907a2be8";
+    sha256 = "1qc703yg0babixi6wshn5wm2kgl5y1drcswgszh4xxzbrwkk9sv7";
+  });
+in rec {
+  all-cabal-hashes = pkgs.runCommand "all-cabal-hashes" {} ''
+    mkdir $out
+    cd $out
+    tar --strip-components 1 -xf ${pkgs.all-cabal-hashes}
+  '';
+
+  cabal2jsonSrc = builtins.fetchTarball {
+    url = "https://github.com/NorfairKing/cabal2json/tarball/8b864d93e3e99eb547a0d377da213a1fae644902";
+    sha256 = "0zd38mzfxz8jxdlcg3fy6gqq7bwpkfann9w0vd6n8aasyz8xfbpj";
+  };
+
+  cabal2jsonFlake = flakeCompat {
+    src = cabal2jsonSrc;
+  };
+
+  cabal2json = cabal2jsonFlake.defaultNix.packages.${pkgs.system}.cabal2json;
+
+  # parse cabal file via IFD
+  fromCabal = file: name: let
+    file' = l.path {path = file;};
+    jsonFile = pkgs.runCommand "${name}.cabal.json" {} ''
+      ${cabal2json}/bin/cabal2json ${file'} > $out
+    '';
+  in
+    l.fromJSON (l.readFile jsonFile);
+
+  fromYaml = file: let
+    file' = l.path {path = file;};
+    jsonFile = pkgs.runCommand "yaml.json" {} ''
+      ${pkgs.yaml2json}/bin/yaml2json < ${file'} > $out
+    '';
+  in
+    l.fromJSON (l.readFile jsonFile);
+
+  batchCabal2Json = candidates: let
+    candidatesJsonStr = l.toJSON candidates;
+    convertOne = name: version: ''
+      cabalFile=${all-cabal-hashes}/${name}/${version}/${name}.cabal
+      if [ -e $cabalFile ]; then
+        echo "converting cabal to json: ${name}-${version}"
+        mkdir -p $out/${name}/${version}
+        ${cabal2json}/bin/cabal2json \
+          $cabalFile \
+          > $out/${name}/${version}/cabal.json
+      else
+        echo "all-cabal-hashes" seems to be outdated
+        exit 1
+      fi
+    '';
+  in
+    pkgs.runCommand "cabal-json-files" {}
+    (l.concatStringsSep "\n"
+      (l.map (c: convertOne c.name c.version) candidates));
+
+  batchCabalData = candidates: let
+    batchJson = batchCabal2Json candidates;
+  in
+    l.mapAttrs
+    (name: _:
+      l.mapAttrs
+      (version: _: l.fromJSON (l.readFile "${batchJson}/${name}/${version}/cabal.json"))
+      (l.readDir "${batchJson}/${name}"))
+    (l.readDir batchJson);
+}
