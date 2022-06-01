a8e2607f1fcbde5b2290cbff3ddd6b31beab28d8
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-06-01 17:33:07 +0300

    fix: update the simple template

--- templates/simple/flake.nix
+++ templates/simple/flake.nix
@@ -1,16 +1,9 @@
 {
   inputs.dream2nix.url = "github:nix-community/dream2nix";
-  outputs = {
-    self,
-    dream2nix,
-  } @ inputs: let
-    dream2nix = inputs.dream2nix.lib.init {
+  outputs = inp:
+    inp.dream2nix.lib.makeFlakeOutputs {
       # modify according to your supported systems
       systems = ["x86_64-linux"];
-      config.projectRoot = ./.;
-    };
-  in
-    dream2nix.makeFlakeOutputs {
       source = ./.;
     };
 }
