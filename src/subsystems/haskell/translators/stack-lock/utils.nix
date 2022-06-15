a65730e23d09787313a72fd43cf4cc185b5c8dbd
Andrea Ciceri <andrea.ciceri@autistici.org>
2022-06-15 15:52:13 +0200

    Using `cabal2json` from nixpkgs instead that from a flake (#176)
    * Nixpkgs input updated
    
    Starting from now `cabal2json` is available in nixpkgs
    
    * Using `cabal2json` from Nixpkgs
    
    * Using more idiomatic `haskellLib` utils
    
    * Formatting

--- src/subsystems/haskell/translators/stack-lock/utils.nix
+++ src/subsystems/haskell/translators/stack-lock/utils.nix
@@ -16,16 +16,25 @@ in rec {
     tar --strip-components 1 -xf ${pkgs.all-cabal-hashes}
   '';
 
-  cabal2jsonSrc = builtins.fetchTarball {
-    url = "https://github.com/NorfairKing/cabal2json/tarball/8b864d93e3e99eb547a0d377da213a1fae644902";
-    sha256 = "0zd38mzfxz8jxdlcg3fy6gqq7bwpkfann9w0vd6n8aasyz8xfbpj";
-  };
-
-  cabal2jsonFlake = flakeCompat {
-    src = cabal2jsonSrc;
-  };
-
-  cabal2json = cabal2jsonFlake.defaultNix.packages.${pkgs.system}.cabal2json;
+  cabal2json = let
+    haskellLib = pkgs.haskell.lib.compose;
+    haskellPackages = pkgs.haskell.packages.ghc8107.override {
+      overrides = _: prev: {
+        autodocodec = haskellLib.markUnbroken prev.autodocodec;
+        validity-aeson = haskellLib.markUnbroken prev.validity-aeson;
+        validity =
+          haskellLib.overrideCabal (_: {
+            patches = [];
+          })
+          prev.validity;
+      };
+    };
+    cabal2json' = haskellPackages.cabal2json.override {
+      Cabal = haskellPackages.Cabal_3_2_1_0;
+    };
+    cabal2json = haskellLib.dontCheck cabal2json';
+  in
+    cabal2json;
 
   # parse cabal file via IFD
   fromCabal = file: name: let
