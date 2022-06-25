fb5d8e32cedf94ca98b9a76ae032eeaa1c17fc68
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-06-25 23:35:57 +0300

    fix: don't discard context from config passed as text

--- src/utils/config.nix
+++ src/utils/config.nix
@@ -7,9 +7,11 @@ let
   #   - attrset (no changes)
   loadAttrs = input:
     if b.isPath input
+    # discarding context here should be fine since we read the text from
+    # a path, which will be realized and nothing else will need to be realized
     then b.fromJSON (b.unsafeDiscardStringContext (b.readFile input))
     else if b.isString input
-    then b.fromJSON (b.unsafeDiscardStringContext input)
+    then b.fromJSON input
     else if b.isAttrs input
     then input
     else throw "input for loadAttrs must be json file or string or attrs";
