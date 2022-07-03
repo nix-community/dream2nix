672eda6f5c679c902a24edd6b183b7cf489f3ed0
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-07-03 19:45:07 +0300

    feat: add makeOutputsForIndexes utility

--- src/utils/index.nix
+++ src/utils/index.nix
@@ -2,8 +2,9 @@
   lib,
   dlib,
   dream2nixInterface,
+  pkgs,
   ...
-}: let
+} @ topArgs: let
   l = lib // builtins;
   defaultMkPackagesFromDreamLock = dreamLock:
     (dream2nixInterface.makeOutputsForDreamLock {
@@ -18,7 +19,7 @@
     (acc: el: acc // el)
     {}
     (l.map makePackagesForDreamLock dreamLocks);
-in {
+in rec {
   generatePackagesFromLocksTree = {
     source ? throw "pass source",
     tree ? dlib.prepareSourceTree {inherit source;},
@@ -48,4 +49,98 @@ in {
     generatePackagesFromLocks {
       inherit dreamLocks makePackagesForDreamLock;
     };
+  makeOutputsForIndexes = {
+    source,
+    indexNames,
+    extendOutputs ? args: prevOutputs: {},
+  }: let
+    l = lib // builtins;
+    mkApp = script: {
+      type = "app";
+      program = toString script;
+    };
+
+    mkIndexApp = {
+      name,
+      input,
+    } @ args: let
+      input = {outputFile = "${name}/index.json";} // args.input;
+      script = pkgs.writers.writeBash "index" ''
+        set -e
+        inputJson="$(${pkgs.coreutils}/bin/mktemp)"
+        echo '${l.toJSON input}' > $inputJson
+        ${d2n.apps.index}/bin/index ${name} $inputJson
+      '';
+    in
+      mkApp script;
+    mkTranslateApp = name:
+      mkApp (
+        pkgs.writers.writeBash "translate-${name}" ''
+          set -e
+          ${d2n.apps.translate-index}/bin/translate-index \
+            ${name}/index.json ${name}/locks
+        ''
+      );
+    translateApps = l.listToAttrs (
+      l.map
+      (
+        name:
+          l.nameValuePair
+          "translate-${name}"
+          (mkTranslateApp name)
+      )
+      indexNames
+    );
+    translateAllApp = let
+      allTranslators =
+        l.concatStringsSep
+        "\n"
+        (
+          l.mapAttrsToList
+          (
+            name: translator: ''
+              echo "::translating with ${name}::"
+              ${translator}
+              echo "::translated with ${name}::"
+            ''
+          )
+          translateApps
+        );
+    in
+      mkApp (
+        pkgs.writers.writeBash "translate-all" ''
+          set -e
+          ${allTranslators}
+        ''
+      );
+
+    mkIndexOutputs = name:
+      if l.pathExists "${source}/${name}/locks"
+      then
+        l.removeAttrs
+        (generatePackagesFromLocksTree {
+          source = l.path {
+            name = "${name}";
+            path = "${source}/${name}/locks";
+          };
+        })
+        ["default"]
+      else {};
+
+    allPackages =
+      l.foldl'
+      (acc: el: acc // el)
+      {}
+      (l.map mkIndexOutputs indexNames);
+
+    outputs = {
+      packages = allPackages;
+      apps =
+        translateApps
+        // {
+          translate = translateAllApp;
+        };
+    };
+  in
+    outputs // (extendOutputs topArgs outputs);
 }
