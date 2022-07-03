b2c0e45ae9b5f4d9d5a761d427cfe2b1bae0a173
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-07-04 00:05:14 +0300

    refactor: improve translate app performance

--- src/apps/translate/default.nix
+++ src/apps/translate/default.nix
@@ -5,18 +5,18 @@
   translateSourceShortcut,
   coreutils,
   moreutils,
-  jq,
   nix,
   python3,
+  jq,
   ...
 }:
 utils.writePureShellScriptBin
 "translate"
 [
+  jq
   coreutils
   moreutils
   nix
-  jq
   python3
 ]
 ''
@@ -55,17 +55,23 @@ utils.writePureShellScriptBin
           p:
           let
             resolve = p.passthru.resolve or p.resolve;
+            mkBashEnv = name: value:
+              \"\''${name}=\" + \"'\" + value + \"'\";
           in
             if \"$translateSkipResolved\" == \"1\" && resolve.passthru.project ? dreamLock
             then null
-            else {
-              inherit (resolve.passthru.project)
-                name
-                dreamLockPath
-                subsystem
-                ;
-              drvPath = resolve.drvPath;
-            }
+            else
+              l.concatStringsSep
+              \";\"
+              (
+                (with resolve.passthru.project; [
+                  (mkBashEnv \"name\" name)
+                  (mkBashEnv \"dreamLockPath\" dreamLockPath)
+                  (mkBashEnv \"subsystem\" subsystem)
+                ]) ++ [
+                  (mkBashEnv \"drvPath\" resolve.drvPath)
+                ]
+              )
         )
         (
           l.attrValues
@@ -84,13 +90,13 @@ utils.writePureShellScriptBin
       l.unique (l.filter (i: i != null) data)
   " > $resolveDatas
 
+  # extract source info once here so we can use it later
+  sourceInfo="$(jq '.' -c -r "$sourceInfoPath")"
+
   # resolve the packages
   for resolveData in $(jq '.[]' -c -r $resolveDatas); do
     # extract project data so we can determine where the dream-lock.json will be
-    name=$(echo "$resolveData" | jq '.name' -c -r)
-    subsystem=$(echo "$resolveData" | jq '.subsystem' -c -r)
-    dreamLockPath="$targetDir/$(echo "$resolveData" | jq '.dreamLockPath' -c -r)"
-    drvPath=$(echo "$resolveData" | jq '.drvPath' -c -r)
+    eval "$resolveData"
 
     echo "Resolving:: $name (subsystem: $subsystem) (lock path: $dreamLockPath)"
 
@@ -99,20 +105,30 @@ utils.writePureShellScriptBin
     $TMPDIR/resolve/bin/resolve
 
     # patch the dream-lock with our source info so the dream-lock works standalone
-    for packageName in $(jq '.sources | keys | .[]' -c -r "$dreamLockPath"); do
-      for packageVersion in $(jq ".sources.\"$packageName\" | keys | .[]" -c -r "$dreamLockPath"); do
-        sourceData="$(jq ".sources.\"$packageName\".\"$packageVersion\"" -c -r "$dreamLockPath")"
-        usesSourceRoot="$(echo "$sourceData" | jq '.rootName == null and .rootVersion == null' -c -r)"
-        if [ "$usesSourceRoot" == "true" ]; then
-          relPath="$(echo "$sourceData" | jq '.relPath' -c -r)"
-          packageSourceInfo="$(jq ".dir = \"$relPath\"" -c -r "$sourceInfoPath")"
-          jq ".sources.\"$packageName\".\"$packageVersion\" = $packageSourceInfo" \
-            -c -r "$dreamLockPath" | sponge "$dreamLockPath"
-        fi
-      done
-    done
-
-    cat "$dreamLockPath" \
+    patchLockQuery="
+      .sources
+      | to_entries
+      | map(.value = (.value | to_entries))
+      | map(
+        .value =
+          (
+            .value
+            | map(
+                if (
+                  .value
+                  | .type == \"path\"
+                    and .rootName == null
+                    and .rootVersion == null
+                )
+                then .value = ($sourceInfo | .dir = .value.relPath)
+                else . end
+              )
+          )
+        )
+      | map(.value = (.value | from_entries))
+      | from_entries
+    "
+    jq ".sources = ($patchLockQuery)" -c -r "$dreamLockPath" \
       | python3 ${../cli/format-dream-lock.py} \
       | sponge "$dreamLockPath"
 
