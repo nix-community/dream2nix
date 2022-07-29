33fdde023350d18debcfbbe9e00909ad4d0b30d6
DavHau <hsngrmpf+github@gmail.com>
2022-07-29 21:23:42 +0200

    feat: indexer: use gnuparallel to speed up index translation

--- src/apps/translate-index/default.nix
+++ src/apps/translate-index/default.nix
@@ -3,30 +3,36 @@
   translate,
   coreutils,
   jq,
+  parallel,
+  writeScript,
   ...
-}:
-utils.writePureShellScriptBin
-"translate-index"
-[coreutils translate jq]
-''
-  cd $WORKDIR
+}: let
+  script = writeScript "run-translate" ''
+    ${translate}/bin/translate $1 $targetDir
+  '';
+in
+  utils.writePureShellScriptBin
+  "translate-index"
+  [coreutils translate jq parallel]
+  ''
+    cd $WORKDIR
 
-  usage="usage:
-    $0 INDEX_PATH TARGET_DIR"
+    usage="usage:
+      $0 INDEX_PATH TARGET_DIR"
 
-  if [ "$#" -ne 2 ]; then
-    echo "error: wrong number of arguments"
-    echo "$usage"
-    exit 1
-  fi
+    if [ "$#" -ne 2 ]; then
+      echo "error: wrong number of arguments"
+      echo "$usage"
+      exit 1
+    fi
 
-  index=''${1:?"error: please pass index file path"}
-  index=$(realpath $index)
-  targetDir=''${2:?"error: please pass a target directory"}
-  targetDir=$(realpath $targetDir)
+    index=''${1:?"error: please pass index file path"}
+    index=$(realpath $index)
+    targetDir=''${2:?"error: please pass a target directory"}
+    targetDir=$(realpath $targetDir)
 
-  for shortcut in $(jq '.[]' -c -r $index); do
-    translate "$shortcut" $targetDir \
-      || echo "failed to translate \"$shortcut\""
-  done
-''
+    export targetDir
+
+    parallel --halt now,fail=1 -j$(nproc) --delay 1 -a <(jq '.[]' -c -r $index) ${script}
+
+  ''
