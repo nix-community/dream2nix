3a170a110c63b2e71a219a6512086fb223dcc576
Wout Mertens <Wout.Mertens@gmail.com>
2022-08-11 14:36:02 +0200

    nodejs builder: implement tree-of-symlinks
    - works out-of-the-box, no node|tsc settings necessary
    - optimal use of storage, composable
    - handles cyclic dependencies by co-locating cycles in the same store path
    
    Changes:
    - build node_modules as a separate derivation, use everywhere
    - if main package has a build script, run it with dev modules and
      afterwards run install script if present, then switch to prod modules
    - only run build scripts when necessary, speedup
    - optionally add all transitive binaries to the node_modules/.bin
    - store binaries in bin/
      NixOS standard location, and .bin should only be used for dependencies,
      not the main package
    - in a package, .bin is now a symlink to bin
    - in bin name, strip .js ending for string case
    - remove now-unnecessary code
    
    This builder requires all peer dependencies for packages to be mentioned and all transitive circular dependencies to be grouped

--- src/subsystems/nodejs/builders/granular/default.nix
+++ src/subsystems/nodejs/builders/granular/default.nix
@@ -45,22 +45,18 @@
     b = builtins;
     l = lib // builtins;
 
-    nodejsVersion = subsystemAttrs.nodejsVersion;
+    nodejsVersion = subsystemAttrs.nodejsVersion or null;
+    transitiveBinaries = subsystemAttrs.transitiveBinaries or false;
 
     isMainPackage = name: version:
       (args.packages."${name}" or null) == version;
 
     nodejs =
-      if args ? nodejs
-      then args.nodejs
-      else
+      args.nodejs
+      or (
         pkgs."nodejs-${builtins.toString nodejsVersion}_x"
-        or (throw "Could not find nodejs version '${nodejsVersion}' in pkgs");
-
-    nodeSources = runCommandLocal "node-sources" {} ''
-      tar --no-same-owner --no-same-permissions -xf ${nodejs.src}
-      mv node-* $out
-    '';
+        or (throw "Could not find nodejs version '${nodejsVersion}' in pkgs")
+      );
 
     allPackages =
       lib.mapAttrs
@@ -148,14 +144,139 @@
 
     # Generates a derivation for a specific package name + version
     makePackage = name: version: let
-      pname = lib.replaceStrings ["@" "/"] ["__at__" "__slash__"] name;
+      pname = name;
 
-      deps = getDependencies name version;
+      rawDeps = getDependencies name version;
+      inherit (getCyclicHelpers name version) cycleeDeps cyclicParent isCyclee isThisCycleeFor replaceCyclees;
 
-      nodeDeps =
-        lib.forEach
-        deps
-        (dep: allPackages."${dep.name}"."${dep.version}");
+      # cycles
+      # for nodejs, we need to copy any cycles into a single package together
+      # getCyclicHelpers already cut the cycles for us, into one cyclic (e.g. eslint) and many cyclee (e.g. eslint-util)
+      # when a package is cyclic:
+      # - the cyclee deps should not be in the cyclic/node_modules folder
+      # - the cyclee deps need to be copied into the package next to cyclic
+      #   so node can find them all together
+      # when a package is cyclee:
+      # - the cyclic dep should not be in the cyclee/node_modules folder
+      # when a dep is cyclee:
+      # - the dep path should point into the cyclic parent
+
+      # Keep only the deps we can install, assume it all works out
+      deps = let
+        myOS = with stdenv.targetPlatform;
+          if isLinux
+          then "linux"
+          else if isDarwin
+          then "darwin"
+          else "";
+      in
+        replaceCyclees (lib.filter
+          (
+            dep: let
+              p = allPackages."${dep.name}"."${dep.version}";
+              s = p.extraInfo;
+            in
+              # this dep is a cyclee
+              !(isCyclee dep.name dep.version)
+              # this dep is not for this os
+              && ((s.os or null == null) || lib.any (o: o == myOS) s.os)
+              # this package is a cyclee
+              && !(isThisCycleeFor dep.name dep.version)
+          )
+          rawDeps);
+
+      nodePkgs =
+        l.map
+        (dep: let
+          pkg = allPackages."${dep.name}"."${dep.version}";
+        in
+          if dep ? replaces
+          then pkg // {packageName = dep.replaces.name;}
+          else pkg)
+        deps;
+      cycleePkgs =
+        l.map
+        (dep: allPackages."${dep.name}"."${dep.version}")
+        cycleeDeps;
+
+      # Derivation building the ./node_modules directory in isolation.
+      makeModules = {
+        withDev ? false,
+        withOptionals ? true,
+      }: let
+        isMain = isMainPackage name version;
+        # These flags will only be present if true. Also, dev deps are required for non-main packages
+        myDeps =
+          lib.filter
+          (dep: let
+            s = dep.extraInfo;
+          in
+            (withOptionals || !(s.optional or false))
+            && (!isMain || (withDev || !(s.dev or false))))
+          nodePkgs;
+      in
+        if lib.length myDeps == 0
+        then null
+        else
+          pkgs.runCommandLocal "node_modules-${pname}" {} ''
+            shopt -s nullglob
+            set -e
+
+            mkdir $out
+
+            function doLink() {
+              local name=$(basename $1)
+              local target="$2/$name"
+              if [ -e "$target" ]; then
+                local link=$(readlink $target)
+                if [ "$link" = $1 ]; then
+                  # cyclic dep, all ok
+                  return
+                fi
+                echo "Cannot overwrite $target (-> $link) with $1 - incorrect cycle! Versions issue?" >&2
+                exit 1
+              fi
+              ln -s $1 $target
+            }
+
+            function linkDep() {
+              local pkg=$1
+              local name=$2
+              # special case for namespaced modules
+              if [[ $name == @* ]]; then
+                local namespace=$(dirname $name)
+                mkdir -p $out/$namespace
+                doLink $pkg/lib/node_modules/$name $out/$namespace
+              else
+                doLink $pkg/lib/node_modules/$name $out
+              fi
+            }
+
+            ${l.toString (l.map
+              (d: "linkDep ${l.toString d} ${d.packageName}\n")
+              myDeps)}
+
+            # symlink module executables to ./node_modules/.bin
+            mkdir $out/.bin
+            for dep in ${l.toString myDeps}; do
+              # We assume dotfiles are not public binaries
+              for b in $dep/bin/*; do
+                if [ -L "$b" ]; then
+                  # when these relative symlinks, make absolute
+                  # last one wins (-sf)
+                  ln -sf $(readlink -f $b) $out/.bin/$(basename $b)
+                else
+                  # e.g. wrapped binary
+                  ln -sf $b $out/.bin/$(basename $b)
+                fi
+              done
+            done
+            # remove empty .bin
+            rmdir $out/.bin || true
+          '';
+      prodModules = makeModules {withDev = false;};
+      # if noDev was used, these are just the prod modules
+      devModules = makeModules {withDev = true;};
 
       passthruDeps =
         l.listToAttrs
@@ -192,13 +313,22 @@
         then null
         else pkgs."electron_${electronVersionMajor}".headers;
 
+      hasExtraInfo = subsystemAttrs ? extraInfo;
+      extraInfo = subsystemAttrs.extraInfo.${name}.${version} or {};
+      # If the translator doesn't provide extraInfo, assume scripts
+      hasInstall =
+        if hasExtraInfo
+        then extraInfo.hasInstallScript or false
+        else true;
+      isMain = isMainPackage name version;
+
       pkg = produceDerivation name (stdenv.mkDerivation rec {
         inherit
           dependenciesJson
           electronHeaders
-          nodeDeps
-          nodeSources
           version
+          transitiveBinaries
+          prodModules
           ;
 
         packageName = name;
@@ -208,52 +338,63 @@
         passthru.dependencies = passthruDeps;
 
         passthru.devShell = import ./devShell.nix {
-          inherit
-            mkShell
-            nodejs
-            packageName
-            pkg
-            ;
+          inherit mkShell nodejs devModules;
         };
+
+        passthru.extraInfo = extraInfo;
 
         /*
         For top-level packages install dependencies as full copies, as this
         reduces errors with build tooling that doesn't cope well with
         symlinking.
         */
-        installMethod =
-          if isMainPackage name version
-          then "copy"
-          else "symlink";
+        # TODO implement copy and make configurable
+        # installMethod =
+        #   if isMain
+        #   then "copy"
+        #   else "symlink";
 
         electronAppDir = ".";
 
         # only run build on the main package
-        runBuild = isMainPackage name version;
-
-        src = getSource name version;
-
-        nativeBuildInputs = [makeWrapper];
-
-        buildInputs = [jq nodejs python3];
-
-        # prevents running into ulimits
-        passAsFile = ["dependenciesJson" "nodeDeps"];
-
-        preConfigurePhases = ["d2nLoadFuncsPhase" "d2nPatchPhase"];
+        runBuild = isMain && (subsystemAttrs.hasBuildScript or true);
 
         # can be overridden to define alternative install command
-        # (defaults to 'npm run postinstall')
+        # (defaults to npm install steps)
         buildScript = null;
+        shouldBuild = hasInstall || runBuild || buildScript != null || electronHeaders != null;
+        buildModules =
+          if runBuild
+          then devModules
+          else prodModules;
+        nodeSources =
+          if shouldBuild
+          then nodejs
+          else null;
+
+        # We don't need unpacked sources
+        src = let t = getSource name version; in t.original or t;
+
+        nativeBuildInputs =
+          if shouldBuild
+          then [makeWrapper]
+          else [];
+
+        # We must provide nodejs even when not building to allow
+        # patchShebangs to find it for binaries
+        buildInputs =
+          if shouldBuild || (!hasExtraInfo || (extraInfo ? bin))
+          then [jq nodejs python3]
+          else [python3];
+
+        # prevents running into ulimits
+        passAsFile = ["dependenciesJson"];
+
+        preConfigurePhases = ["d2nLoadFuncsPhase" "d2nPatchPhase"];
 
         # python script to modify some metadata to support installation
         # (see comments below on d2nPatchPhase)
         fixPackage = "${./fix-package.py}";
-
-        # script to install (symlink or copy) dependencies.
-        installDeps = "${./install-deps.py}";
-
-        # python script to link bin entries from package.json
         linkBins = "${./link-bins.py}";
 
         # costs performance and doesn't seem beneficial in most scenarios
@@ -325,27 +466,29 @@
               mv -- "$strippedName" "$sourceRoot"
           fi
 
+          # provide bin, we'll remove it if unused
+          mkdir $out/bin
+          # We keep the binaries in /bin but node uses .bin
+          # Symlink so that wrapper scripts etc work
+          ln -s ../../bin $nodeModules/.bin
+
           runHook postUnpack
         '';
 
         # The python script wich is executed in this phase:
         #   - ensures that the package is compatible to the current system
+        #     (if not already filtered above with os prop from translator)
         #   - ensures the main version in package.json matches the expected
         #   - pins dependency versions in package.json
         #     (some npm commands might otherwise trigger networking)
         #   - creates symlinks for executables declared in package.json
+        #   - Any usage of 'link:' in deps will be replaced with the exact version
         # Apart from that:
-        #   - Any usage of 'link:' in package.json is replaced with 'file:'
         #   - If package-lock.json exists, it is deleted, as it might conflict
         #     with the parent package-lock.json.
         d2nPatchPhase = ''
           # delete package-lock.json as it can lead to conflicts
           rm -f package-lock.json
-
-          # repair 'link:' -> 'file:'
-          mv $nodeModules/$packageName/package.json $nodeModules/$packageName/package.json.old
-          cat $nodeModules/$packageName/package.json.old | sed 's!link:!file\:!g' > $nodeModules/$packageName/package.json
-          rm $nodeModules/$packageName/package.json.old
 
           # run python script (see comment above):
           python $fixPackage \
@@ -359,48 +502,44 @@
           else
             exit 1
           fi
-
-          # configure typescript
-          if [ -f ./tsconfig.json ] \
-              && node -e 'require("typescript")' &>/dev/null; then
-            node ${./tsconfig-to-json.js}
-            ${pkgs.jq}/bin/jq ".compilerOptions.preserveSymlinks = true" tsconfig.json \
-                | ${pkgs.moreutils}/bin/sponge tsconfig.json
-          fi
         '';
 
-        # - installs dependencies into the node_modules directory
-        # - adds executables of direct node module dependencies to PATH
-        # - adds the current node module to NODE_PATH
+        # - links dependencies into the node_modules directory + adds bin to PATH
         # - sets HOME=$TMPDIR, as this is required by some npm scripts
-        # TODO: don't install dev dependencies. Load into NODE_PATH instead
         configurePhase = ''
           runHook preConfigure
 
-          # symlink sub dependencies as well as this imitates npm better
-          python $installDeps
-
-          echo "Symlinking transitive executables to $nodeModules/.bin"
-          for dep in ${l.toString nodeDeps}; do
-            binDir=$dep/lib/node_modules/.bin
-            if [ -e $binDir ]; then
-              for bin in $(ls $binDir/); do
-                mkdir -p $nodeModules/.bin
-
-                # symlink might have been already created by install-deps.py
-                # if installMethod=copy was selected
-                if [ ! -e $nodeModules/.bin/$bin ]; then
-                  ln -s $binDir/$bin $nodeModules/.bin/$bin
+          if [ -n "$buildModules" ]; then
+            if [ -L $sourceRoot/node_modules ] || [ -e $sourceRoot/node_modules ]; then
+              echo Warning: The source $sourceRoot includes a node_modules directory. Replacing. >&2
+              rm -rf $sourceRoot/node_modules
+            fi
+            ln -s $buildModules $sourceRoot/node_modules
+            if [ -d $buildModules/.bin ]; then
+              export PATH="$PATH:$sourceRoot/node_modules/.bin"
+            fi
+          fi
+          ${
+            # Here we copy cyclee deps into the cyclehead node_modules
+            # so the cyclic deps can find each other
+            if cycleePkgs != []
+            then ''
+              for dep in ${l.toString cycleePkgs}; do
+                # We must copy everything so Node finds it
+                # Let's hope that clashing names are just duplicates
+                # keep write perms with no-preserve
+                cp -rf --no-preserve=mode $dep/lib/node_modules/* $nodeModules
+                if [ -d $dep/bin ]; then
+                  # this copies symlinks as-is, so they will point to the
+                  # local target when relative, and module-local links
+                  # are made relative by nixpkgs post-build
+                  # last one wins (-f)
+                  cp -af --no-preserve=mode $dep/bin/. $out/bin/.
                 fi
               done
-            fi
-          done
-
-          # add bin path entries collected by python script
-          export PATH="$PATH:$nodeModules/.bin"
-
-          # add dependencies to NODE_PATH
-          export NODE_PATH="$NODE_PATH:$nodeModules/$packageName/node_modules"
+            ''
+            else ""
+          }
 
           export HOME=$TMPDIR
 
@@ -409,46 +548,76 @@
 
         # Runs the install command which defaults to 'npm run postinstall'.
         # Allows using custom install command by overriding 'buildScript'.
-        buildPhase = ''
-          runHook preBuild
+        # TODO this logic supposes a build script, which is not documented
+        # for installing, we only need to run `npm run install` (pre and post scripts run automatically)
+        # https://github.com/npm/npm/issues/5919
+        # TODO build first if has build, give it devModules during build
 
-          # execute electron-rebuild
-          if [ -n "$electronHeaders" ]; then
-            echo "executing electron-rebuild"
-            ${electron-rebuild}
-          fi
+        buildPhase =
+          if shouldBuild
+          then ''
+            set -x
+            runHook preBuild
 
-          # execute install command
-          if [ -n "$buildScript" ]; then
-            if [ -f "$buildScript" ]; then
-              $buildScript
-            else
-              eval "$buildScript"
-            fi
-          # by default, only for top level packages, `npm run build` is executed
-          elif [ -n "$runBuild" ] && [ "$(jq '.scripts.build' ./package.json)" != "null" ]; then
-            npm run build
-          else
-            if [ "$(jq '.scripts.preinstall' ./package.json)" != "null" ]; then
-              npm --production --offline --nodedir=$nodeSources run preinstall
-            fi
-            if [ "$(jq '.scripts.install' ./package.json)" != "null" ]; then
-              npm --production --offline --nodedir=$nodeSources run install
-            fi
-            if [ "$(jq '.scripts.postinstall' ./package.json)" != "null" ]; then
-              npm --production --offline --nodedir=$nodeSources run postinstall
-            fi
-          fi
+            if [ -n "$shouldBuild" ]; then
+              # execute electron-rebuild
+              if [ -n "$electronHeaders" ]; then
+                echo "executing electron-rebuild"
+                ${electron-rebuild}
+              fi
 
-          runHook postBuild
-        '';
+              # execute install command
+              if [ -n "$buildScript" ]; then
+                if [ -f "$buildScript" ]; then
+                  $buildScript
+                else
+                  eval "$buildScript"
+                fi
+              else
+                if [ -n "$runBuild" ]; then
+                  # by default, only for top level packages, `npm run build` is executed
+                  npm run --if-present build
+                fi
+
+                # This seems to be the only script that needs running on install
+                npm --omit=dev --offline --nodedir=$nodeSources run --if-present install
+              fi
+            fi
+
+            runHook postBuild
+            set +x
+          ''
+          else "true";
 
         # Symlinks executables and manual pages to correct directories
         installPhase = ''
           runHook preInstall
 
+          if [ "$buildModules" != "$prodModules" ]; then
+            if [ -n "$prodModules" ]; then
+              ln -sf $prodModules $sourceRoot/node_modules
+            else
+              rm $sourceRoot/node_modules
+            fi
+          fi
+
           echo "Symlinking bin entries from package.json"
           python $linkBins
+
+          if [ -n "$transitiveBinaries" ]; then
+            # pass down transitive binaries, like npm does
+            # all links are absolute so we can just copy
+            cp -af --no-preserve=mode $prodModules/.bin/. $out/bin/.
+          fi
+
+          if rmdir $out/bin 2>/dev/null; then
+            # we didn't install any binaries
+            rm $nodeModules/.bin
+          else
+            # make sure binaries are executable, following symlinks
+            # ignore failures from symlinks pointing to other pkgs
+            chmod a+x $out/bin/* 2>/dev/null || true
+          fi
 
           echo "Symlinking manual pages"
           if [ -d "$nodeModules/$packageName/man" ]
