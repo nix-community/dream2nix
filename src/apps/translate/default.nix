fac674e43f16eca6cdd143d1301e639323f87a81
DavHau <hsngrmpf+github@gmail.com>
2022-08-21 16:25:39 +0200

    WIP: indexers: make use of source-less translators
    Previously the output of an indexer was a list of sources.
    Since #259, dream2nix supports translation without sources.
    This change updates the indexers to make support that feature.
    With this change, the output of each indexer will be a list of project specifications instead of sources.
    
    The new model:
      - Covers more use cases
      - Allows to translate from arbitrary data. For example {translator="apt"; name="htop"} could be a valid project specification.
      - Moves the logic for fetching inside the translator instead of using dream2nix fetchers. (less calls to nix)
      - does not use dream2nix makeFlakeOutputs logic in order to execute translation scripts (This is better, as depending on our high level abstrations would make refactoring/deprecating such more difficult)

--- src/apps/translate/default.nix
+++ src/apps/translate/default.nix
@@ -1,5 +1,6 @@
 {
   # dream2nix deps
+  framework,
   utils,
   callNixWithD2N,
   translateSourceShortcut,
@@ -21,7 +22,7 @@ utils.writePureShellScriptBin
 ]
 ''
   usage="usage:
-    $0 SOURCE_SHORTCUT TARGET_DIR"
+    $0 PROJECT_JSON TARGET_DIR"
 
   if [ "$#" -ne 2 ]; then
     echo "error: wrong number of arguments"
@@ -29,91 +30,43 @@ utils.writePureShellScriptBin
     exit 1
   fi
 
-  source=''${1:?"error: pass a source shortcut"}
+  projectJson=''${1:?"error: pass projects spec as json string"}
   targetDir=''${2:?"error: please pass a target directory"}
   targetDir="$(realpath "$targetDir")"
-
-  sourceInfoPath=''${translateSourceInfoPath:-"$TMPDIR/sourceInfo.json"}
-  translateSkipResolved=''${translateSkipResolved:-"0"}
+  translator=$(jq '.translator' -c -r <(echo $projectJson))
+  name=$(jq '.name' -c -r <(echo $projectJson))
+  id=$(jq '.id' -c -r <(echo $projectJson))
+  if [ "$id" == "null" ]; then
+    echo "error: 'id' field not specified for project $name"
+    exit 1
+  fi
+  dreamLockPath="$targetDir/$id/dream-lock.json"
 
   mkdir -p $targetDir && cd $targetDir
 
-  export dream2nixConfig="{packagesDir=\"./.\"; projectRoot=\"$targetDir\";}"
+  echo -e "\nTranslating:: $name (translator: $translator) (lock path: $dreamLockPath)"
 
-  # translate the source shortcut
-  ${translateSourceShortcut} $source > $sourceInfoPath
+  translateBin=$(${callNixWithD2N} build --print-out-paths --no-link "
+    dream2nix.framework.translatorInstances.$translator.translateBin
+  ")
 
-  # collect data for the packages we will resolve
-  resolveDatas="$TMPDIR/resolveData.json"
-  ${callNixWithD2N} eval --json "
-    let
-      data =
-        l.map
-        (
-          p:
-          let
-            resolve = p.passthru.resolve or p.resolve;
-            mkBashEnv = name: value:
-              \"\''${name}=\" + \"'\" + value + \"'\";
-          in
-            # skip this project if we don't want to resolve ones that can be resolved on the fly
-            if \"$translateSkipResolved\" == \"1\" && resolve.passthru.project ? dreamLock
-            then null
-            else
-              # write a simple bash script for exporting the data we need
-              # this is better since we don't need to call jq multiple times
-              # to access the data we need
-              l.concatStringsSep
-              \";\"
-              (
-                (with resolve.passthru.project; [
-                  (mkBashEnv \"name\" name)
-                  (mkBashEnv \"dreamLockPath\" dreamLockPath)
-                  (mkBashEnv \"subsystem\" subsystem)
-                ]) ++ [
-                  (mkBashEnv \"drvPath\" resolve.drvPath)
-                ]
-              )
-        )
-        (
-          l.attrValues
-          (
-            l.removeAttrs
-            (dream2nix.makeOutputs {
-              source = dream2nix.fetchers.fetchSource {
-                source =
-                  l.fromJSON (l.readFile \"$sourceInfoPath\");
-              };
-            }).packages
-            [\"resolveImpure\"]
-          )
-        );
-    in
-      l.unique (l.filter (i: i != null) data)
-  " > $resolveDatas
+  echo "
+    {
+      \"project\": $projectJson,
+      \"outputFile\": \"$dreamLockPath\"
+    }
+  " > $TMPDIR/args.json
 
-  # resolve the packages
-  for resolveData in $(jq '.[]' -c -r $resolveDatas); do
-    # extract project data so we can determine where the dream-lock.json will be
-    eval "$resolveData"
+  $translateBin $TMPDIR/args.json
 
-    echo "Resolving:: $name (subsystem: $subsystem) (lock path: $dreamLockPath)"
+  cat $dreamLockPath \
+    | python3 ${../cli/format-dream-lock.py} \
+    | sponge $dreamLockPath
 
-    # build the resolve script and run it
-    nix build --out-link $TMPDIR/resolve $drvPath
-    $TMPDIR/resolve/bin/resolve
+  ${python3.pkgs.jsonschema}/bin/jsonschema \
+      --instance $dreamLockPath \
+      --output pretty \
+      ${../../specifications/dream-lock-schema.json}
 
-    # patch the dream-lock with our source info so the dream-lock works standalone
-    ${callNixWithD2N} eval --json "
-      with dream2nix.utils.dreamLock;
-      replaceRootSources {
-        dreamLock = l.fromJSON (l.readFile \"$targetDir/$dreamLockPath\");
-        newSourceRoot = l.fromJSON (l.readFile \"$sourceInfoPath\");
-      }
-    " \
-      | python3 ${../cli/format-dream-lock.py} \
-      | sponge "$dreamLockPath"
-
-    echo "Resolved:: $name (subsystem: $subsystem) (lock path: $dreamLockPath)"
-  done
+  echo -e "\nFinished:: $name (translator: $translator) (lock path: $dreamLockPath)"
 ''
---
+++ src/subsystems/nodejs/translators/npm/default.nix
@@ -0,0 +1,101 @@
+{
+  dlib,
+  lib,
+  ...
+}: {
+  type = "impure";
+
+  # the input format is specified in /specifications/translator-call-example.json
+  # this script receives a json file including the input paths and specialArgs
+  translateBin = {
+    # dream2nix utils
+    apps,
+    subsystems,
+    utils,
+    # nixpkgs dependenies
+    bash,
+    coreutils,
+    git,
+    jq,
+    moreutils,
+    nodePackages,
+    openssh,
+    writeScriptBin,
+    ...
+  }:
+    utils.writePureShellScript
+    [
+      bash
+      coreutils
+      git
+      jq
+      moreutils
+      nodePackages.npm
+      openssh
+    ]
+    ''
+      # accroding to the spec, the translator reads the input from a json file
+      jsonInput=$1
+
+      # read the json input
+      outputFile=$(realpath -m $(jq '.outputFile' -c -r $jsonInput))
+      name=$(jq '.project.name' -c -r $jsonInput)
+      version=$(jq '.project.version' -c -r $jsonInput)
+      npmArgs=$(jq '.project.subsystemInfo.npmArgs' -c -r $jsonInput)
+
+      if [ "$version" = "null" ]; then
+        candidate="$name"
+      else
+        candidate="$name@$version"
+      fi
+
+
+      pushd $TMPDIR
+      newSource=$(pwd)
+
+      npm install $candidate --package-lock-only $npmArgs
+
+      jq ".source = \"$newSource\"" -c -r $jsonInput > $TMPDIR/newJsonInput
+      jq ".project.relPath = \"\"" -c -r $TMPDIR/newJsonInput | sponge $TMPDIR/newJsonInput
+
+      popd
+
+      # call package-lock translator
+      ${subsystems.nodejs.translators.package-lock.translateBin} $TMPDIR/newJsonInput
+
+      # generate source info for main package
+      url=$(npm view $candidate dist.tarball)
+      hash=$(npm view $candidate dist.integrity)
+      echo "
+        {
+          \"type\": \"http\",
+          \"url\": \"$url\",
+          \"hash\": \"$hash\"
+        }
+      " > $TMPDIR/sourceInfo.json
+
+      # add main package source info to dream-lock.json
+      ${apps.callNixWithD2N} eval --json "
+        with dream2nix.utils.dreamLock;
+        replaceRootSources {
+          dreamLock = l.fromJSON (l.readFile \"$outputFile\");
+          newSourceRoot = l.fromJSON (l.readFile \"$TMPDIR/sourceInfo.json\");
+        }
+      " \
+        | sponge "$outputFile"
+    '';
+
+  # inherit options from package-lock translator
+  extraArgs =
+    dlib.translators.translators.nodejs.package-lock.extraArgs
+    // {
+      npmArgs = {
+        description = "Additional arguments for npm";
+        type = "argument";
+        default = "";
+        examples = [
+          "--force"
+        ];
+      };
+    };
+}
