fefba13b47ccc97a0cb735ead9740292046b3235
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-09-02 03:21:44 +0300

    fix(rust): add missing crane hook

--- src/default.nix
+++ src/default.nix
@@ -136,6 +136,7 @@ in let
       }: rec {
         otherHooks =
           genHooks [
+            "cargoHelperFunctionsHook"
             "configureCargoCommonVarsHook"
             "configureCargoVendoredDepsHook"
           ]
