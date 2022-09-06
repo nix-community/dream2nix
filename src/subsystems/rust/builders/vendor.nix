806e9e07a90e4445ee4ecc1bd286f0d57ee684b5
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-09-07 01:20:13 +0300

    fix(rust/crane): copy vendor directory recursively as symlinks so that we don't copy the entire directory but also make sure crates still can write to their source

--- src/subsystems/rust/builders/vendor.nix
+++ src/subsystems/rust/builders/vendor.nix
@@ -98,5 +98,5 @@ in rec {
   # All dependencies in the Cargo.lock file, vendored
   vendoredDependencies = vendorDependencies allDependencies;
 
-  copyVendorDir = path: ''cp -r --no-preserve=mode,ownership ${vendoredDependencies} ${path}'';
+  copyVendorDir = path: ''cp -rs --no-preserve=mode,ownership ${vendoredDependencies} ${path}'';
 }
