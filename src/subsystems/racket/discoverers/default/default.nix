a6502bd52e8674579c6c9ccd933faf62e76205b0
Brian Leung <leungbk@posteo.net>
2022-09-11 16:33:35 -0700

    Add Racket discoverer

---
+++ src/subsystems/racket/discoverers/default/default.nix
@@ -0,0 +1,37 @@
+{
+  dlib,
+  lib,
+  subsystem,
+  ...
+}: let
+  l = lib // builtins;
+
+  discover = {
+    tree,
+    topLevel ? true,
+  }:
+    if (tree ? files."info.rkt")
+    then [
+      (dlib.construct.discoveredProject {
+        inherit subsystem;
+        relPath = tree.relPath;
+        name =
+          if topLevel
+          then "main"
+          else
+            l.unsafeDiscardStringContext
+            (l.last
+              (l.splitString "/" (l.removeSuffix "/" "${tree.fullPath}")));
+        translators = ["racket-impure"];
+        subsystemInfo = {};
+      })
+    ]
+    else
+      l.flatten (l.mapAttrsToList (_dirName: dirTree:
+        discover {
+          tree = dirTree;
+          topLevel = false;
+        }) (tree.directories or {}));
+in {
+  inherit discover;
+}
