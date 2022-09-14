c17d995e8db44d0b10b20007273ee9c427f179da
Brian Leung <leungbk@posteo.net>
2022-09-14 04:15:09 -0700

    Add impure Racket translator

---
+++ examples/racket/flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    dream2nix.url = "github:nix-community/dream2nix";
+
+    goblins.url = "gitlab:leungbk/goblins";
+    goblins.flake = false;
+  };
+
+  outputs = {
+    self,
+    dream2nix,
+    goblins,
+  } @ inp: (dream2nix.lib.makeFlakeOutputs {
+    systems = ["x86_64-linux"];
+    config.projectRoot = ./.;
+    source = goblins;
+  });
+}
