74128bba5d3b119b5ca6e28b4535692689b05681
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-09-27 21:52:46 +0300

    fix: remove extra usage from _d2n-extended-new-subsystem

--- examples/_d2n-extended-new-subsystem/flake.nix
+++ examples/_d2n-extended-new-subsystem/flake.nix
@@ -10,7 +10,6 @@
     (dream2nix.lib.makeFlakeOutputs {
       systems = ["x86_64-linux"];
       config.projectRoot = ./.;
-      config.extra = ./extra.nix;
       config.modules = [
         ./discoverers.nix
         ./translators.nix
