024239d0cb4385860f2a2d567b7e1eb3731b215a
Michael Schneider <michael.schneider@cyberus-technology.de>
2022-10-22 10:56:26 +0200

    wip

---
+++ examples/haskell_stack_multiproject/flake.nix
@@ -0,0 +1,9 @@
+{
+  inputs.dream2nix.url = "github:nix-community/dream2nix";
+  outputs = inp:
+    inp.dream2nix.lib.makeFlakeOutputs {
+      systemsFromFile = ./nix_systems;
+      config.projectRoot = ./.;
+      source = ./.;
+    };
+}
