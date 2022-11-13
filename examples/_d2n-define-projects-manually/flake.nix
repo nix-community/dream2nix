1eec3ec74f72d849a91b7464f66d5be5b412bb3a
DavHau <hsngrmpf+github@gmail.com>
2022-11-13 16:00:32 +0100

    feat: add example for defining projects manually

---
+++ examples/_d2n-define-projects-manually/flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    dream2nix.url = "github:nix-community/dream2nix";
+    nixpkgs.follows = "dream2nix/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    src.url = "github:prettier/prettier/2.4.1";
+    src.flake = false;
+  };
+
+  outputs = {
+    self,
+    dream2nix,
+    flake-parts,
+    src,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit self;} {
+      systems = ["x86_64-linux"];
+      imports = [dream2nix.flakeModuleBeta];
+
+      perSystem = {config, ...}: {
+        # define an input for dream2nix to generate outputs for
+        dream2nix.inputs."prettier" = {
+          source = src;
+          projects = {
+            prettier = {
+              name = "prettier";
+              subsystem = "nodejs";
+              translator = "yarn-lock";
+            };
+          };
+        };
+      };
+    };
+}
