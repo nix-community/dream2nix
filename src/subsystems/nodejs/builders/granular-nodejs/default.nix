0da40528da4e94e4ccbaded3fdcecbd7fe7198ee
hsjobeki <hsjobeki@gmail.com>
2022-11-16 11:30:11 +0100

    nodejs/builder: refactor phases into modules. remove unneeded custom phases

--- src/subsystems/nodejs/builders/granular-nodejs/default.nix
+++ src/subsystems/nodejs/builders/granular-nodejs/default.nix
@@ -88,58 +88,6 @@
           );
       };
 
-      # This is only executed for electron based packages.
-      # Electron ships its own version of node, requiring a rebuild of native
-      # extensions.
-      # Theoretically this requires headers for the exact electron version in use,
-      # but we use the headers from nixpkgs' electron instead which might have a
-      # different minor version.
-      # Alternatively the headers can be specified via `electronHeaders`.
-      # Also a custom electron version can be specified via `electronPackage`
-      electron-rebuild = ''
-        # prepare node headers for electron
-        if [ -n "$electronPackage" ]; then
-          export electronDist="$electronPackage/lib/electron"
-        else
-          export electronDist="$nodeModules/$packageName/node_modules/electron/dist"
-        fi
-        local ver
-        ver="v$(cat $electronDist/version | tr -d '\n')"
-        mkdir $TMP/$ver
-        cp $electronHeaders $TMP/$ver/node-$ver-headers.tar.gz
-
-        # calc checksums
-        cd $TMP/$ver
-        sha256sum ./* > SHASUMS256.txt
-        cd -
-
-        # serve headers via http
-        python -m http.server 45034 --directory $TMP &
-
-        # copy electron distribution
-        cp -r $electronDist $TMP/electron
-        chmod -R +w $TMP/electron
-
-        # configure electron toolchain
-        ${pkgs.jq}/bin/jq ".build.electronDist = \"$TMP/electron\"" package.json \
-            | ${pkgs.moreutils}/bin/sponge package.json
-
-        ${pkgs.jq}/bin/jq ".build.linux.target = \"dir\"" package.json \
-            | ${pkgs.moreutils}/bin/sponge package.json
-
-        ${pkgs.jq}/bin/jq ".build.npmRebuild = false" package.json \
-            | ${pkgs.moreutils}/bin/sponge package.json
-
-        # execute electron-rebuild if available
-        export headers=http://localhost:45034/
-        if command -v electron-rebuild &> /dev/null; then
-          pushd $electronAppDir
-
-          electron-rebuild -d $headers
-          popd
-        fi
-      '';
-
       # Generates a derivation for a specific package name + version
       makePackage = name: version: let
         pname = lib.replaceStrings ["@" "/"] ["__at__" "__slash__"] name;
@@ -242,8 +190,6 @@
           # prevents running into ulimits
           passAsFile = ["dependenciesJson" "nodeDeps"];
 
-          preConfigurePhases = ["d2nLoadFuncsPhase" "d2nPatchPhase"];
-
           # can be overridden to define alternative install command
           # (defaults to 'npm run postinstall')
           buildScript = null;
@@ -293,171 +239,22 @@
             then "tar --delay-directory-restore -xf $src"
             else null;
 
-          unpackPhase = ''
-            runHook preUnpack
-
-            nodeModules=$(realpath ./package)
-
-            export sourceRoot="$nodeModules/$packageName"
-
-            # sometimes tarballs do not end with .tar.??
-            unpackFallback(){
-              local fn="$1"
-              tar xf "$fn"
-            }
-
-            unpackCmdHooks+=(unpackFallback)
-
-            unpackFile $src
-
-            # Make the base dir in which the target dependency resides in first
-            mkdir -p "$(dirname "$sourceRoot")"
-
-            # install source
-            if [ -f "$src" ]
-            then
-                # Figure out what directory has been unpacked
-                packageDir="$(find . -maxdepth 1 -type d | tail -1)"
-
-                # Restore write permissions
-                find "$packageDir" -type d -exec chmod u+x {} \;
-                chmod -R u+w -- "$packageDir"
-
-                # Move the extracted tarball into the output folder
-                mv -- "$packageDir" "$sourceRoot"
-            elif [ -d "$src" ]
-            then
-                strippedName="$(stripHash $src)"
-
-                # Restore write permissions
-                chmod -R u+w -- "$strippedName"
-
-                # Move the extracted directory into the output folder
-                mv -- "$strippedName" "$sourceRoot"
-            fi
-
-            runHook postUnpack
-          '';
-
-          # The python script wich is executed in this phase:
-          #   - ensures that the package is compatible to the current system
-          #   - ensures the main version in package.json matches the expected
-          #   - pins dependency versions in package.json
-          #     (some npm commands might otherwise trigger networking)
-          #   - creates symlinks for executables declared in package.json
-          # Apart from that:
-          #   - Any usage of 'link:' in package.json is replaced with 'file:'
-          #   - If package-lock.json exists, it is deleted, as it might conflict
-          #     with the parent package-lock.json.
-          d2nPatchPhase = ''
-            # delete package-lock.json as it can lead to conflicts
-            rm -f package-lock.json
-
-            # repair 'link:' -> 'file:'
-            mv $nodeModules/$packageName/package.json $nodeModules/$packageName/package.json.old
-            cat $nodeModules/$packageName/package.json.old | sed 's!link:!file\:!g' > $nodeModules/$packageName/package.json
-            rm $nodeModules/$packageName/package.json.old
-
-            # run python script (see comment above):
-            cp package.json package.json.bak
-            python $fixPackage \
-            || \
-            # exit code 3 -> the package is incompatible to the current platform
-            #  -> Let the build succeed, but don't create lib/node_modules
-            if [ "$?" == "3" ]; then
-              mkdir -p $out
-              echo "Not compatible with system $system" > $out/error
-              exit 0
-            else
-              exit 1
-            fi
-
-            # configure typescript
-            if [ -f ./tsconfig.json ] \
-                && node -e 'require("typescript")' &>/dev/null; then
-              node ${./tsconfig-to-json.js}
-              ${pkgs.jq}/bin/jq ".compilerOptions.preserveSymlinks = true" tsconfig.json \
-                  | ${pkgs.moreutils}/bin/sponge tsconfig.json
-            fi
-          '';
+          unpackPhase = import ./unpackPhase.nix {};
 
           # - installs dependencies into the node_modules directory
           # - adds executables of direct node module dependencies to PATH
           # - adds the current node module to NODE_PATH
           # - sets HOME=$TMPDIR, as this is required by some npm scripts
           # TODO: don't install dev dependencies. Load into NODE_PATH instead
-          configurePhase = ''
-            runHook preConfigure
-
-            # symlink sub dependencies as well as this imitates npm better
-            python $installDeps
-
-            echo "Symlinking transitive executables to $nodeModules/.bin"
-            for dep in ${l.toString nodeDeps}; do
-              binDir=$dep/lib/node_modules/.bin
-              if [ -e $binDir ]; then
-                for bin in $(ls $binDir/); do\
-                  if [ ! -e $nodeModules/.bin ]; then
-                    mkdir -p $nodeModules/.bin
-                  fi
-
-                  # symlink might have been already created by install-deps.py
-                  # if installMethod=copy was selected
-                  if [ ! -L $nodeModules/.bin/$bin ]; then
-                    ln -s $binDir/$bin $nodeModules/.bin/$bin
-                  else
-                    echo "won't overwrite existing symlink $nodeModules/.bin/$bin. current target: $(readlink $nodeModules/.bin/$bin)"
-                  fi
-                done
-              fi
-            done
-
-            # add bin path entries collected by python script
-            export PATH="$PATH:$nodeModules/.bin"
-
-            # add dependencies to NODE_PATH
-            export NODE_PATH="$NODE_PATH:$nodeModules/$packageName/node_modules"
-
-            export HOME=$TMPDIR
-
-            runHook postConfigure
-          '';
+          configurePhase = import ./configurePhase.nix {
+            inherit lib nodeDeps;
+          };
 
           # Runs the install command which defaults to 'npm run postinstall'.
           # Allows using custom install command by overriding 'buildScript'.
-          buildPhase = ''
-            runHook preBuild
-
-            # execute electron-rebuild
-            if [ -n "$electronHeaders" ]; then
-              echo "executing electron-rebuild"
-              ${electron-rebuild}
-            fi
-
-            # execute install command
-            if [ -n "$buildScript" ]; then
-              if [ -f "$buildScript" ]; then
-                $buildScript
-              else
-                eval "$buildScript"
-              fi
-            # by default, only for top level packages, `npm run build` is executed
-            elif [ -n "$runBuild" ] && [ "$(jq '.scripts.build' ./package.json)" != "null" ]; then
-              npm run build
-            else
-              if [ "$(jq '.scripts.preinstall' ./package.json)" != "null" ]; then
-                npm --production --offline --nodedir=$nodeSources run preinstall
-              fi
-              if [ "$(jq '.scripts.install' ./package.json)" != "null" ]; then
-                npm --production --offline --nodedir=$nodeSources run install
-              fi
-              if [ "$(jq '.scripts.postinstall' ./package.json)" != "null" ]; then
-                npm --production --offline --nodedir=$nodeSources run postinstall
-              fi
-            fi
-
-            runHook postBuild
-          '';
+          buildPhase = import ./buildPhase.nix {
+            inherit pkgs;
+          };
 
           # Symlinks executables and manual pages to correct directories
           installPhase = import ./installPhase.nix {
