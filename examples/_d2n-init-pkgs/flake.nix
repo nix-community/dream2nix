ecc71875f5fe08bd4a59c8b79f037f86d6a26999
DavHau <hsngrmpf+github@gmail.com>
2022-12-01 19:15:30 +0700

    chore: limit API surface; remove makeOutputs from examples

--- examples/_d2n-init-pkgs/flake.nix
+++ examples/_d2n-init-pkgs/flake.nix
@@ -10,34 +10,19 @@
 
     allPkgs =
       l.map
+      # nixpkgs could be imported manually here with overrides etc.
       (system: inp.dream2nix.inputs.nixpkgs.legacyPackages.${system})
       ["x86_64-linux" "aarch64-linux"];
-
-    initD2N = pkgs:
-      inp.dream2nix.lib.init {
-        inherit pkgs;
-        config.projectRoot = ./.;
-      };
-
-    makeOutputs = pkgs: let
-      outputs = (initD2N pkgs).dream2nix-interface.makeOutputs {
-        source = inp.src;
-        settings = [
-          {
-            builder = "build-rust-package";
-            translator = "cargo-lock";
-          }
-        ];
-      };
-    in rec {
-      packages.${pkgs.system} = outputs.packages;
-      # checks.${pkgs.system} = {
-      #   inherit (outputs.packages) linemd;
-      # };
-    };
-
-    allOutputs = l.map makeOutputs allPkgs;
-    outputs = l.foldl' l.recursiveUpdate {} allOutputs;
   in
-    outputs;
+    inp.dream2nix.lib.makeFlakeOutputs {
+      pkgs = allPkgs;
+      config.projectRoot = ./.;
+      source = inp.src;
+      settings = [
+        {
+          builder = "build-rust-package";
+          translator = "cargo-lock";
+        }
+      ];
+    };
 }
