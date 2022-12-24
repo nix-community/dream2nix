03fe970dcf727d4b89cad60fedf02c2be449dbc6
David Arnold <david.arnold@iohk.io>
2022-12-23 19:57:44 -0500

    wip: a terrible idea

--- examples/_d2n-std/nix/app/packages.nix
+++ examples/_d2n-std/nix/app/packages.nix
@@ -3,14 +3,20 @@
   cell,
 }: {
   default = cell.packages.app;
-  app = {
-    source = inputs.src;
-    projects = {
-      prettier = {
-        name = "prettier";
-        subsystem = "nodejs";
-        translator = "yarn-lock";
+  app =
+    # a terrible idea
+    (inputs.dream2nix.lib.makeFlakeOutputs {
+      systems = [inputs.nixpkgs.system];
+      source = inputs.src;
+      projects = {
+        prettier = {
+          name = "prettier";
+          subsystem = "nodejs";
+          translator = "yarn-lock";
+        };
       };
-    };
-  };
+    })
+    .packages
+    .${inputs.nixpkgs.system}
+    .default;
 }
