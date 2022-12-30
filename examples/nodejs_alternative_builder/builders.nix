c65e34d386e25188b7ead3ee5edc6188cf68ee77
hsjobeki <hsjobeki+github@gmail.com>
2022-12-30 22:51:54 +0100

    remove: leftover example file

--- examples/nodejs_alternative_builder/builders.nix
+++
@@ -1,7 +0,0 @@
-{dream2nix}: {config, ...}: {
-  builders.nodejs = {...}: {
-    name = "nodejs-strict-builder";
-    subsystem = "nodejs";
-    imports = [(attrs: import "${dream2nix}/src/subsystems/nodejs/builders/strict-builder" attrs.framework)];
-  };
-}
