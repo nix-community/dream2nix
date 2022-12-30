9e2f23d654e5d517d71dd0c2f76f974be597d666
Erin <79354991+oati@users.noreply.github.com>
2022-12-30 15:39:27 -0600

    haskell: cabal translator: basic example based on the cabal-plan example

---
+++ examples/haskell_cabal/flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    dream2nix.url = "github:nix-community/dream2nix";
+    src.url = "github:davhau/cabal2json/plan-json";
+    src.flake = false;
+  };
+
+  outputs = {
+    self,
+    dream2nix,
+    src,
+  } @ inp:
+    (dream2nix.lib.makeFlakeOutputs {
+      pkgs = dream2nix.inputs.nixpkgs.legacyPackages.x86_64-linux;
+      source = src;
+      config.projectRoot = ./.;
+      projects = ./projects.toml;
+    })
+    // {
+      # checks.x86_64-linux.cabal2json = self.packages.x86_64-linux.cabal2json.overrideAttrs (old: {
+      #   doCheck = false;
+      # });
+    };
+}
