f47ce48d15c910bc5ac5fd46424c198ad0fdeb42
DavHau <hsngrmpf+github@gmail.com>
2023-02-06 14:35:45 +0700

    fix: formatting

--- src/subsystems/haskell/translators/cabal/fixup-dream-lock.py
+++ src/subsystems/haskell/translators/cabal/fixup-dream-lock.py
@@ -3,12 +3,12 @@ import os
 import sys
 
 lock = json.load(sys.stdin)
-source = os.environ.get('source')
+source = os.environ.get("source")
 
-name = lock['_generic']['defaultPackage']
-version = lock['_generic']['packages'][name]
+name = lock["_generic"]["defaultPackage"]
+version = lock["_generic"]["packages"][name]
 
 # follow the original source file
-lock['sources'][name][version]['path'] = source
+lock["sources"][name][version]["path"] = source
 
 print(json.dumps(lock, indent=2))
