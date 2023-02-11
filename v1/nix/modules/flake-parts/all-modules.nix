90fe5a0d7e744c218060cf8ab9593024bd22a973
DavHau <hsngrmpf+github@gmail.com>
2023-02-11 17:16:17 +0700

    init

---
+++ v1/nix/modules/flake-parts/all-modules.nix
@@ -0,0 +1,35 @@
+{config, lib, ...}: let
+
+  modulesDir = ../.;
+
+  moduleKinds = builtins.readDir modulesDir;
+
+  mapModules = kind:
+    lib.mapAttrs'
+    (fn: _:
+      lib.nameValuePair
+      (lib.removeSuffix ".nix" fn)
+      (modulesDir + "/${kind}/${fn}"))
+    (builtins.readDir (modulesDir + "/${kind}"));
+
+  flakePartsModules = lib.attrValues (
+    lib.filterAttrs
+    (modName: _: modName != "all-modules")
+    (mapModules "flake-parts")
+  );
+
+in {
+
+  imports = flakePartsModules;
+
+  options.flake.modules = lib.mkOption {
+    type = lib.types.anything;
+  };
+
+  # generates future flake outputs: `modules.<kind>.<module-name>`
+  config.flake.modules = lib.mapAttrs (kind: _: mapModules kind) moduleKinds;
+
+  # comapt to current schema: `nixosModules` / `darwinModules`
+  config.flake.nixosModules = config.flake.modules.nixos or {};
+  config.flake.darwinModules = config.flake.modules.darwin or {};
+}
