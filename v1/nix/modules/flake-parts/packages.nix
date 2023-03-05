05f35fcbb73c8f03bd001560bd34a6e7f35a1b2b
DavHau <hsngrmpf+github@gmail.com>
2023-03-05 14:15:38 +0700

    chore: add comments to packages.nix

--- v1/nix/modules/flake-parts/packages.nix
+++ v1/nix/modules/flake-parts/packages.nix
@@ -1,18 +1,21 @@
 { self, lib, inputs, ... }: {
   perSystem = { config, self', inputs', pkgs, system, ... }: let
 
+    # A module imported into every package setting up the eval cache
     evalCacheSetup = {config,...}: {
       eval-cache.cacheFileRel = "/nix/modules/drvs/${config.public.name}/cache-${system}.json";
       eval-cache.repoRoot = self;
       eval-cache.enable = true;
     };
 
+    # evalautes the package behind a given module
     makeDrv = module: let
       evaled = lib.evalModules {
         modules = [
           inputs.drv-parts.modules.drv-parts.core
           inputs.drv-parts.modules.drv-parts.docs
           module
+          ../drv-parts/eval-cache
           evalCacheSetup
         ];
         specialArgs.dependencySets = {
@@ -25,6 +28,7 @@
       evaled // evaled.config.public;
 
   in {
+    # map all modules in ../drvs to a package output in the flake.
     packages = lib.mapAttrs (_: drvModule: makeDrv drvModule) self.modules.drvs;
   };
 }
