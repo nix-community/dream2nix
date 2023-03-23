96873569bf87c81ba5a90e675b5b38a5a2212c14
DavHau <hsngrmpf+github@gmail.com>
2023-03-23 14:41:22 +0800

    feat(mach-nix): use fetch-pip and set defaults

--- v1/nix/modules/drvs/ansible/default.nix
+++ v1/nix/modules/drvs/ansible/default.nix
@@ -10,11 +10,6 @@ in {
     ../../drv-parts/mach-nix-xs
   ];
 
-  # use lock file to manage hash for fetchPip
-  lock.fields.fetchPipHash =
-    config.lock.lib.computeFODHash
-    config.mach-nix.pythonSources;
-
   deps = {nixpkgs, ...}: {
     python = nixpkgs.python39;
     inherit (nixpkgs.writers) writePython3;
@@ -38,12 +33,9 @@ in {
   };
 
   mach-nix.pythonSources = {
-    imports = [../../drv-parts/fetch-pip];
-    deps.python = config.deps.python;
     fetch-pip = {
       maxDate = "2023-01-01";
       requirementsList = ["${config.name}==${config.version}"];
-      hash = config.lock.content.fetchPipHash;
     };
   };
 }
