b23e6f39ec90375814fcc56f1f82fa186d59899a
DavHau <hsngrmpf+github@gmail.com>
2023-03-28 16:38:24 +0800

    feat(lib.evalModules): init convenience wrapper for lib.evalModules

---
+++ v1/nix/modules/flake-parts/lib.evalModules.nix
@@ -0,0 +1,46 @@
+{
+  self,
+  lib,
+  inputs,
+  ...
+}: {
+  flake.options.lib = lib.mkOption {
+    type = lib.types.lazyAttrsOf lib.types.raw;
+  };
+  flake.config.lib.evalModules = args @ {
+    packageSets,
+    modules,
+    # Ff set, returns the result coming form nixpgs.lib.evalModules as is,
+    # otherwise it returns the derivation only (.config.public).
+    raw ? false,
+    ...
+  }: let
+    forawardedArgs = builtins.removeAttrs args [
+      "packageSets"
+      "return"
+    ];
+
+    evaluated =
+      lib.evalModules
+      (
+        forawardedArgs
+        // {
+          modules =
+            args.modules
+            ++ [
+              inputs.drv-parts.modules.drv-parts.core
+            ];
+          specialArgs = {
+            inherit packageSets;
+            dream2nix.modules.drv-parts = self.modules.drv-parts;
+          };
+        }
+      );
+
+    result =
+      if raw
+      then evaluated
+      else evaluated.config.public;
+  in
+    result;
+}
