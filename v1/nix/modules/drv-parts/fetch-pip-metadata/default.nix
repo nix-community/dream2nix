88e2f0f416fe7010a40475d5cb6c18dde8a46c77
phaer <hello@phaer.org>
2023-03-31 13:32:48 +0200

    feat(fetchPipMetadata): add drv-parts module

---
+++ v1/nix/modules/drv-parts/fetch-pip-metadata/default.nix
@@ -0,0 +1,52 @@
+{
+  config,
+  lib,
+  drv-parts,
+  name,
+  version,
+  ...
+}: let
+  l = lib // builtins;
+
+  fetchPip = import ../../../pkgs/fetchPipMetadata {
+    inherit lib;
+    inherit
+      (config.deps)
+      buildPackages
+      stdenv
+      python3 # only used for proxy script
+      ;
+  };
+in {
+  imports = [
+    ./interface.nix
+    ../lock
+    drv-parts.modules.drv-parts.mkDerivation
+  ];
+
+  inherit name version;
+
+  package-func.outputs = ["out" "names"];
+
+  deps = {nixpkgs, ...}:
+    l.mapAttrs (_: l.mkDefault) {
+      inherit
+        (nixpkgs)
+        buildPackages
+        stdenv
+        python3 # only used for proxy script
+        ;
+      python = nixpkgs.python3;
+    };
+
+  package-func.func = fetchPip;
+  package-func.args = l.mkForce (
+    config.fetch-pip-metadata
+    // {
+      inherit (config) name;
+    }
+    // lib.optionalAttrs (config.mkDerivation.nativeBuildInputs != null) {
+      inherit (config.mkDerivation) nativeBuildInputs;
+    }
+  );
+}
