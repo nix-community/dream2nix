1b0bc39a2bc5a8e1040b959e0bd54e2f4bc18647
phaer <hello@phaer.org>
2023-04-20 12:12:26 +0200

    tests(v1): init pytest runner

---
+++ v1/nix/modules/flake-parts/tests.nix
@@ -0,0 +1,34 @@
+# custom app to run our test suite
+{
+  self,
+  lib,
+  inputs,
+  ...
+}: {
+  imports = [
+    ./writers.nix
+  ];
+  perSystem = {
+    config,
+    self',
+    inputs',
+    pkgs,
+    system,
+    ...
+  }: let
+    python = pkgs.python3.withPackages (p: [
+      p.pytest
+    ]);
+    script =
+      config.writers.writePureShellScriptBin "test-runner"
+      []
+      ''
+        ${python}/bin/python -m pytest $@
+      '';
+  in {
+    apps.test = {
+      type = "app";
+      program = "${script}/bin/test-runner";
+    };
+  };
+}
