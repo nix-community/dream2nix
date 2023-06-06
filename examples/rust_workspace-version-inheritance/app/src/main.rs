c6edea84ae7f40aeeb03932c6d304320e299c26e
Grégoire Geis <opensource@gregoirege.is>
2023-06-06 09:52:26 +0000

    rust: add support for workspace-inherited crate versions

---
+++ examples/rust_workspace-version-inheritance/app/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("hello from v{}", env!("CARGO_PKG_VERSION"));
+}
--- src/subsystems/rust/findAllCrates.nix
+++ src/subsystems/rust/findAllCrates.nix
@@ -3,24 +3,40 @@
   dlib,
   ...
 }: let
-  discoverCrates = {tree}: let
+  discoverCrates = {
+    tree,
+    workspaceVersion,
+  }: let
     cargoToml = tree.files."Cargo.toml".tomlContent or {};
 
     subdirCrates =
       lib.flatten
       (lib.mapAttrsToList
-        (dirName: dir: discoverCrates {tree = dir;})
+        (dirName: dir:
+          discoverCrates {
+            inherit workspaceVersion;
+            tree = dir;
+          })
         (tree.directories or {}));
   in
     if cargoToml ? package.name
     then
       [
         {
-          inherit (cargoToml.package) name version;
+          inherit (cargoToml.package) name;
           inherit (tree) relPath;
+
+          version =
+            if cargoToml.package.version.workspace or false
+            then workspaceVersion
+            else cargoToml.package.version;
         }
       ]
       ++ subdirCrates
     else subdirCrates;
 in
-  discoverCrates
+  {tree}:
+    discoverCrates {
+      inherit tree;
+      workspaceVersion = tree.files."Cargo.toml".tomlContent.workspace.package.version or null;
+    }
