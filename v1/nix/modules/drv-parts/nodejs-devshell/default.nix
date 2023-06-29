d67e213ed35b47509d564911081f142b7b4b6e63
DavHau <hsngrmpf+github@gmail.com>
2023-06-29 17:06:07 +0300

    feat(v1): new modules nodejs-devshell

---
+++ v1/nix/modules/drv-parts/nodejs-devshell/default.nix
@@ -0,0 +1,57 @@
+{
+  config,
+  lib,
+  dream2nix,
+  packageSets,
+  ...
+}: let
+  l = lib // builtins;
+
+  cfg = config.nodejs-devshell;
+
+  nodeModulesDrv = dream2nix.lib.evalModules {
+    inherit packageSets;
+    modules = [
+      dream2nix.modules.drv-parts.nodejs-node-modules
+      {inherit (config) nodejs-package-lock name version;}
+      {mkDerivation.src = l.mkForce null;}
+    ];
+  };
+
+  nodeModulesDir = "${nodeModulesDrv}/lib/node_modules/${config.name}/node_modules";
+  binDir = "${nodeModulesDrv}/lib/node_modules/.bin";
+in {
+  imports = [
+    dream2nix.modules.drv-parts.mkDerivation
+    dream2nix.modules.drv-parts.nodejs-package-lock
+  ];
+
+  # rsync the node_modules folder
+  # - tracks node-modules store path via .dream2nix/.node_modules_id
+  # - omits copying if store path did not change
+  # - if store path changes, only replaces updated files
+  # - rsync can be restarted from any point, if failed or aborted mid execution.
+  # Options:
+  # -a            -> all files recursive, preserve symlinks, etc.
+  # --delete      -> removes deleted files
+  # --chmod=+ug+w -> make folder writeable by user+group
+  mkDerivation.shellHook = ''
+    ID=${nodeModulesDir}
+    currID=$(cat .dream2nix/.node_modules_id 2> /dev/null)
+
+    mkdir -p .dream2nix
+    if [[ "$ID" != "$currID" || ! -d "node_modules"  ]];
+    then
+      ${config.deps.rsync}/bin/rsync -a --chmod=ug+w  --delete ${nodeModulesDir}/ ./node_modules/
+      mkdir -p ./node_modules/.bin
+      for executablePath in ${binDir}/*; do
+        binaryName=$(basename $executablePath)
+        target=$(realpath $executablePath)
+        echo linking binary $binaryName to nix store: $target
+        ln -s $target ./node_modules/.bin/$binaryName
+      done
+      echo -n $ID > .dream2nix/.node_modules_id
+      echo "Ok: node_modules updated"
+    fi
+  '';
+}
---
+++ v1/nix/modules/drvs/prettier-devshell/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  l = lib // builtins;
+in {
+  imports = [
+    ../../drv-parts/nodejs-devshell
+  ];
+
+  mkDerivation = {
+    src = config.deps.fetchFromGitHub {
+      owner = "davhau";
+      repo = "prettier";
+      rev = "2.8.7-package-lock";
+      sha256 = "sha256-zo+WRV3VHja8/noC+iPydtbte93s5GGc3cYaQgNhlEY=";
+    };
+  };
+
+  deps = {nixpkgs, ...}: {
+    inherit
+      (nixpkgs)
+      fetchFromGitHub
+      mkShell
+      rsync
+      stdenv
+      ;
+  };
+
+  name = l.mkForce "prettier";
+  version = l.mkForce "2.8.7";
+}
