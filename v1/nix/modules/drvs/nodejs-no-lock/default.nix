540de4ec52107be7b23da1bf2eb63560ef00a5d1
DavHau <hsngrmpf+github@gmail.com>
2023-07-08 13:03:12 +0200

    examples(nodejs): add example nodejs-no-lock

---
+++ v1/nix/modules/drvs/nodejs-no-lock/default.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  l = lib // builtins;
+  system = config.deps.stdenv.system;
+in {
+  imports = [
+    ../../drv-parts/nodejs-package-json
+    ../../drv-parts/nodejs-granular
+  ];
+
+  mkDerivation = {
+    src = lib.cleanSource ./.;
+    checkPhase = ''
+      ./app.js | ${config.deps.gnugrep}/bin/grep -q "Hello, World!"
+    '';
+    doCheck = true;
+  };
+
+  nodejs-granular = {
+    buildScript = ''
+      tsc ./app.ts
+      mv app.js app.js.tmp
+      echo "#!${config.deps.nodejs}/bin/node" > app.js
+      cat app.js.tmp >> app.js
+      chmod +x ./app.js
+      patchShebangs .
+    '';
+  };
+
+  deps = {nixpkgs, ...}: {
+    inherit
+      (nixpkgs)
+      gnugrep
+      stdenv
+      ;
+  };
+
+  name = l.mkForce "app";
+  version = l.mkForce "0.0.0";
+
+  lock.lockFileRel =
+    l.mkForce "/v1/nix/modules/drvs/nodejs-no-lock/lock-${system}.json";
+}
