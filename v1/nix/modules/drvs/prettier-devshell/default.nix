e6a1c222aebdf6f88f660cc52367d9f9a3570cfa
DavHau <hsngrmpf+github@gmail.com>
2023-07-08 14:25:07 +0200

    examples(nodejs): allow prettier-devshell derivation to be built

--- v1/nix/modules/drvs/prettier-devshell/default.nix
+++ v1/nix/modules/drvs/prettier-devshell/default.nix
@@ -16,6 +16,8 @@ in {
       rev = "2.8.7-package-lock";
       sha256 = "sha256-zo+WRV3VHja8/noC+iPydtbte93s5GGc3cYaQgNhlEY=";
     };
+    # allow devshell to be built -> CI pipeline happy
+    buildPhase = "mkdir $out";
   };
 
   deps = {nixpkgs, ...}: {
