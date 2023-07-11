90f81105a70d33042f3ed689491a06507470fe0a
phaer <hello@phaer.org>
2023-07-11 18:27:47 +0200

    v1/ci: add x86_64-darwin and aarch64-darwin

--- ci.nix
+++ ci.nix
@@ -3,9 +3,28 @@ let
   flakeCompatSrc = b.fetchurl "https://raw.githubusercontent.com/edolstra/flake-compat/12c64ca55c1014cdc1b16ed5a804aa8576601ff2/default.nix";
   flake = (import flakeCompatSrc {src = ./.;}).defaultNix;
   pkgs = import flake.inputs.nixpkgs {};
-  recurseIntoAll = b.mapAttrs (name: val: pkgs.recurseIntoAttrs val);
+  mapRecurse = attrs: f:
+    b.mapAttrs f attrs
+    // {
+      recurseForDerivations = true;
+    };
 in
-  recurseIntoAll {
-    checks = flake.checks.x86_64-linux;
-    packages = flake.packages.x86_64-linux;
+  mapRecurse
+  {
+    x86_64-linux = {};
+    x86_64-darwin = {};
+    aarch64-darwin = {
+      knownIssues = {
+        "odoo" = "TODO github issue link here";
+      };
+    };
   }
+  (
+    system: {knownIssues ? {}}:
+      mapRecurse
+      {
+        checks = flake.checks.${system};
+        packages = pkgs.lib.filterAttrs (n: v: ! b.hasAttr n knownIssues) flake.packages.${system};
+      }
+      (_name: val: pkgs.recurseIntoAttrs val)
+  )
