726f80e77f765f1da84f66769ef6b872243fb279
DavHau <hsngrmpf+github@gmail.com>
2023-08-07 13:50:27 +0200

    benchmarks: pkg-funcs vs. modules

---
+++ misc/benchmarks/benchmark.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+export NUM_PKGS=${1:-1000}
+
+# measure date ini milliseconds
+echo -e "\nBenchmarking ${NUM_PKGS}x builtins.derivaton via pkg-funcs"
+time nix eval --impure -f ./builtins-derivation-modules-vs-pkg-func.nix --json pkg-funcs > /dev/null
+
+echo -e "\nBenchmarking ${NUM_PKGS}x builtins.derivaton via modules"
+time nix eval --impure -f ./builtins-derivation-modules-vs-pkg-func.nix --json modules > /dev/null
+
+echo -e "\nBenchmarking ${NUM_PKGS}x mkDerivation via pkg-funcs"
+time nix eval --impure -f ./mkDerivation-modules-vs-pkg-func.nix --json pkg-funcs > /dev/null
+
+echo -e "\nBenchmarking ${NUM_PKGS}x mkDerivation via modules"
+time nix eval --impure -f ./mkDerivation-modules-vs-pkg-func.nix --json modules > /dev/null
