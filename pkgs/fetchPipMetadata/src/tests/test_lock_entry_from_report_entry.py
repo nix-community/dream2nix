cf1ab45f22549755efe8fbb7286fc1e6c6fa7803
DavHau <hsngrmpf+github@gmail.com>
2023-09-03 01:49:48 +0200

    Revert "Add test for pip with git dependency"
    This reverts commit d2885f9e035beb1d9b8502c5018a85aadb94f434.

--- pkgs/fetchPipMetadata/src/tests/test_lock_entry_from_report_entry.py
+++ pkgs/fetchPipMetadata/src/tests/test_lock_entry_from_report_entry.py
@@ -96,33 +96,3 @@ def test_path_in_nix_store():
         sha256=None,
     )
     assert l.lock_entry_from_report_entry(install) == expected
-
-
-def test_git(monkeypatch):
-    def nix_prefetch_git(url, rev):
-        return "f1bd065cf727c988b605787fd9f75a827a210a2b2ad56965f7d04e9ef80bcd7c"
-
-    monkeypatch.setattr(l, "nix_prefetch_git", nix_prefetch_git)
-
-    install = {
-        "download_info": {
-            "url": "https://github.com/python/mypy",
-            "vcs_info": {
-                "vcs": "git",
-                "commit_id": "df4717ee2cbbeb9e47fbd0e60edcaa6f81bbd7bb",
-            },
-        },
-        "metadata": {
-            "metadata_version": "2.1",
-            "name": "mypy",
-            "version": "1.7.0+dev.df4717ee2cbbeb9e47fbd0e60edcaa6f81bbd7bb",
-        },
-    }
-    expected = "mypy", {
-        "rev": "df4717ee2cbbeb9e47fbd0e60edcaa6f81bbd7bb",
-        "sha256": "f1bd065cf727c988b605787fd9f75a827a210a2b2ad56965f7d04e9ef80bcd7c",
-        "type": "git",
-        "url": "https://github.com/python/mypy",
-        "version": "1.7.0+dev.df4717ee2cbbeb9e47fbd0e60edcaa6f81bbd7bb",
-    }
-    assert l.lock_entry_from_report_entry(install) == expected
