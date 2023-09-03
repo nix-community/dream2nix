ddbd3a159b3a87f4bf22dc7f1cdba4450063ffeb
DavHau <hsngrmpf+github@gmail.com>
2023-09-03 03:09:12 +0200

    example-repo: use dream2nix.lib.evalModules

--- examples/dream2nix-repo/default.nix
+++ examples/dream2nix-repo/default.nix
@@ -9,21 +9,15 @@
   nixpkgs = import dream2nix.inputs.nixpkgs {};
   lib = nixpkgs.lib;
 
-  _callModule = module:
-    nixpkgs.lib.evalModules {
-      specialArgs.dream2nix = dream2nix;
-      specialArgs.packageSets.nixpkgs = nixpkgs;
-      modules = [module ./settings.nix dream2nix.modules.dream2nix.core];
-    };
-
-  # like callPackage for modules
-  callModule = module: (_callModule module).config.public;
-
   packageModuleNames = builtins.attrNames (builtins.readDir ./packages);
 
   packages =
     lib.genAttrs packageModuleNames
-    (moduleName: callModule "${./packages}/${moduleName}");
+    (moduleName:
+      dream2nix.lib.evalModules {
+        modules = ["${./packages}/${moduleName}"];
+        packageSets.nixpkgs = nixpkgs;
+      });
 in
   # all packages defined inside ./packages/
   packages
