2ab02224162d0dfac69e2d44c5e40e71793d485c
DavHau <hsngrmpf+github@gmail.com>
2023-09-12 00:30:00 +0200

    feat(ui): add .lock attribute
    This allows updating the lock file via `nix run .#{package}.lock` instead of ` nix run .#{package}.config.lock.refresh`
    
    This is better as it's shorter, and the keyword `lock` is widely understood to lock dependencies.

--- modules/dream2nix/core/ui/default.nix
+++ modules/dream2nix/core/ui/default.nix
@@ -5,4 +5,10 @@
 
   config.public.name = config.name;
   config.public.version = config.version;
+  config.public.${
+    if config ? lock
+    then "lock"
+    else null
+  } =
+    config.lock.refresh;
 }
