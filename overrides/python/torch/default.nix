77eeb06927e94bc196444fcee2d08a20514443a3
DavHau <hsngrmpf+github@gmail.com>
2023-09-13 00:01:25 +0200

    feat(overlays/torch): add /run/opengl-driver/lib to RPATH
    This allows resolving libcuda.so.1

--- overrides/python/torch/default.nix
+++ overrides/python/torch/default.nix
@@ -3,6 +3,15 @@
   lib,
   ...
 }: {
-  env.autoPatchelfIgnoreMissingDeps = ["libcuda.so.1"];
+  # stripping doesn't reduce the file size much, and it takes a long time
   mkDerivation.dontStrip = true;
+
+  # this file is patched manually, so ignore it in autoPatchelf
+  env.autoPatchelfIgnoreMissingDeps = ["libcuda.so.1"];
+  # patch the rpath so libcuda.so.1 can be found at /run/opengl-driver/lib
+  env.cudaPatchPhase = ''
+    patchelf $out/${config.deps.python.sitePackages}/torch/lib/libcaffe2_nvrtc.so \
+      --add-rpath /run/opengl-driver/lib
+  '';
+  mkDerivation.postPhases = ["cudaPatchPhase"];
 }
