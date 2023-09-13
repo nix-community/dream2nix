3de6990ba653fa2361d8177ad457d9376123db7b
Jairo Llopis <jairo@moduon.team>
2023-09-13 11:11:45 +0100

    docs: simplify python project example
    Use `lib.importTOML`, clearer than the previous implementation.
    
    Reuse more information from `pyproject`.

--- examples/packages/single-language/python-project/default.nix
+++ examples/packages/single-language/python-project/default.nix
@@ -5,17 +5,13 @@
   dream2nix,
   ...
 }: let
-  pyproject = lib.pipe (config.mkDerivation.src + /pyproject.toml) [
-    builtins.readFile
-    builtins.fromTOML
-  ];
+  pyproject = lib.importTOML (config.mkDerivation.src + /pyproject.toml);
 in {
   imports = [
     dream2nix.modules.dream2nix.pip
   ];
 
-  name = "pyproject-dependencies";
-  version = pyproject.project.version;
+  inherit (pyproject.project) name version;
 
   mkDerivation = {
     src = ./.;
@@ -30,7 +26,10 @@ in {
 
   pip = {
     pypiSnapshotDate = "2023-08-27";
-    requirementsList = ["setuptools"] ++ pyproject.project.dependencies;
+    requirementsList =
+      pyproject.build-system.requires
+      or []
+      ++ pyproject.project.dependencies;
     flattenDependencies = true;
   };
 }
