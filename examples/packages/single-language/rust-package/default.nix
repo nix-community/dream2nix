ff13f72090f03e9eb1073fa272da6cc2d30bbd76
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2023-09-17 12:36:00 +0300

    refactor: update rust-package example to latest changes

--- examples/packages/single-language/rust-package/default.nix
+++ examples/packages/single-language/rust-package/default.nix
@@ -16,6 +16,7 @@
   name = lib.mkForce "ripgrep";
   version = lib.mkForce "13.0.0";
 
+  # options defined on top-level will be applied to the main derivation (the derivation that is exposed)
   mkDerivation = {
     # define the source root that contains the package we want to build.
     src = config.deps.fetchFromGitHub {
@@ -24,13 +25,14 @@
       rev = config.version;
       sha256 = "sha256-udEh+Re2PeO3DnX4fQThsaT1Y3MBHFfrX5Q5EN2XrF0=";
     };
-    # note: any more options defined here will be applied to both
-    # `config.mkDerivation` and `config.depsDrv.mkDerivation`.
   };
 
   rust-crane = {
     buildProfile = "dev";
     buildFlags = ["--verbose"];
     runTests = false;
+    depsDrv = {
+      # options defined here will be applied to the dependencies derivation
+    };
   };
 }
