e913ec992b8c0aefa4839caff8eda89f09973384
DavHau <hsngrmpf+github@gmail.com>
2023-09-20 22:03:03 +0200

    ci: remove ./ci.nix

--- modules/flake-parts/checks.nix-unit.nix
+++ modules/flake-parts/checks.nix-unit.nix
@@ -3,18 +3,21 @@
   perSystem = {
     pkgs,
     inputs',
+    lib,
+    system,
     ...
-  }: {
-    # map all modules in /examples to a package output in the flake.
-    checks.nix-unit = pkgs.runCommand "nix-unit-tests" {} ''
-      export NIX_PATH=nixpkgs=${pkgs.path}
-      for test in ${self}/tests/nix-unit/*; do
-        echo -e "Executing tests from file $test"
-        ${inputs'.nix-unit.packages.nix-unit}/bin/nix-unit \
-          "$test" \
-          --eval-store $(realpath .)
-      done
-      touch $out
-    '';
-  };
+  }:
+    lib.optionalAttrs (system == "x86_64-linux") {
+      # map all modules in /examples to a package output in the flake.
+      checks.nix-unit = pkgs.runCommand "nix-unit-tests" {} ''
+        export NIX_PATH=nixpkgs=${pkgs.path}
+        for test in ${self}/tests/nix-unit/*; do
+          echo -e "Executing tests from file $test"
+          ${inputs'.nix-unit.packages.nix-unit}/bin/nix-unit \
+            "$test" \
+            --eval-store $(realpath .)
+        done
+        touch $out
+      '';
+    };
 }
