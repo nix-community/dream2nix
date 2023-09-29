e4a9da41ad98885e36501f73c9bbe604f3cb83e0
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-09-29 23:32:39 +0200

    WIP

---
+++ examples/packages/single-language/spago-project/default.nix
@@ -0,0 +1,15 @@
+{
+  dream2nix,
+  ...
+}: {
+  imports = [
+    dream2nix.modules.dream2nix.WIP-spago
+  ];
+
+  name = "test";
+  version = "1.0.0";
+
+  mkDerivation = {
+    src = ./.;
+  };
+}
