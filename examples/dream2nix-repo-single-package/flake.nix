3ff2ab238dcf29d7e9d84b85db6a2b61c893006d
DavHau <hsngrmpf+github@gmail.com>
2023-10-10 18:54:51 +0200

    findCycles: add some unit tests

---
+++ examples/dream2nix-repo-single-package/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "My flake with dream2nix packages";
+
+  inputs = {
+    dream2nix.url = "github:nix-community/dream2nix";
+    nixpkgs.follows = "dream2nix/nixpkgs";
+  };
+
+  outputs = inputs @ {
+    self,
+    dream2nix,
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+  in {
+    # all packages defined inside ./packages/
+    packages.${system}.default = nixpkgs.lib.evalModules {
+      modules = [];
+
+      projectRoot = ./.;
+      # can be changed to ".git" or "flake.nix" to get rid of .project-root
+      projectRootFile = "flake.nix";
+      packagesDir = ./packages;
+      packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
+    };
+  };
+}
