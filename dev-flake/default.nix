f93e524b9399b215aed61af12f5b9a875fabaabe
DavHau <hsngrmpf+github@gmail.com>
2023-11-05 10:20:09 +0700

    flake-compat: vendor + customize
    modified to fix laziness issue in defaultNix

--- dev-flake/default.nix
+++ dev-flake/default.nix
@@ -2,15 +2,7 @@
 let
   flake =
     import
-    (
-      let
-        lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-      in
-        fetchTarball {
-          url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-          sha256 = lock.nodes.flake-compat.locked.narHash;
-        }
-    )
+    ./flake-compat.nix
     {src = ./.;};
 in
   flake.defaultNix
