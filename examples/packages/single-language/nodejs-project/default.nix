8db4cb11214ec2c415c85632461da0083f3b0a7a
DavHau <hsngrmpf+github@gmail.com>
2023-11-15 15:38:51 +0700

    examples: minimize nodejs-project example

--- examples/packages/single-language/nodejs-project/default.nix
+++ examples/packages/single-language/nodejs-project/default.nix
@@ -3,9 +3,7 @@
   config,
   dream2nix,
   ...
-}: let
-  system = config.deps.stdenv.system;
-in {
+}: {
   imports = [
     dream2nix.modules.dream2nix.nodejs-package-json-v3
     dream2nix.modules.dream2nix.nodejs-granular-v3
@@ -32,9 +30,6 @@ in {
 
   name = lib.mkForce "app";
   version = lib.mkForce "1.0.0";
-
-  lock.lockFileRel =
-    lib.mkForce "/locks/example-package-nodejs-no-lock/lock-${system}.json";
 
   mkDerivation = {
     src = lib.cleanSource ./.;
