e25425d42672469d30968201be35772e9ac69396
DavHau <hsngrmpf+github@gmail.com>
2024-01-07 19:57:57 +0700

    pip: add test can-build-setuptools
    to ensure that a setuptools dependency doesn't raise a collision

---
+++ modules/dream2nix/pip/tests/packages/can-build-setuptools/default.nix
@@ -0,0 +1,42 @@
+# An example package with dependencies defined via pyproject.toml
+{
+  config,
+  lib,
+  dream2nix,
+  ...
+}: let
+  pyproject = lib.importTOML (config.mkDerivation.src + /pyproject.toml);
+in {
+  imports = [
+    dream2nix.modules.dream2nix.pip
+  ];
+
+  deps = {nixpkgs, ...}: {
+    python = nixpkgs.python310;
+  };
+
+  inherit (pyproject.project) name version;
+
+  mkDerivation = {
+    src = ./.;
+    propagatedBuildInputs = [
+      config.pip.drvs.setuptools.public
+    ];
+  };
+
+  buildPythonPackage = {
+    format = lib.mkForce "pyproject";
+    pythonImportsCheck = [
+      "my_tool"
+    ];
+  };
+
+  pip = {
+    pypiSnapshotDate = "2023-08-27";
+    requirementsList =
+      pyproject.build-system.requires
+      or []
+      ++ pyproject.project.dependencies;
+    flattenDependencies = true;
+  };
+}
---
+++ modules/dream2nix/pip/tests/packages/can-build-setuptools/my_tool/__init__.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
