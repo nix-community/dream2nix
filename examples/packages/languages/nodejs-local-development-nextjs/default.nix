42838c590971da17a4b6483962707b7fb7b8b9a7
DavHau <hsngrmpf+github@gmail.com>
2024-03-07 21:16:44 +0700

    examples: cleanup and rename to simplify - name example more consistently, eg. `{language}-packaging-{feature}` or `{language}-local-development-{feature}` - move some examples to the modules integration tests directory instead as their purpose was mainly testing and they weren't good example - module owned checks: import via flake if available

---
+++ examples/packages/languages/nodejs-local-development-nextjs/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  config,
+  dream2nix,
+  ...
+}: {
+  imports = [
+    dream2nix.modules.dream2nix.nodejs-package-lock-v3
+    dream2nix.modules.dream2nix.nodejs-granular-v3
+  ];
+
+  mkDerivation = {
+    src = ./nextjs-app;
+  };
+
+  deps = {nixpkgs, ...}: {
+    inherit
+      (nixpkgs)
+      fetchFromGitHub
+      stdenv
+      ;
+  };
+
+  nodejs-package-lock-v3 = {
+    packageLockFile = "${config.mkDerivation.src}/package-lock.json";
+  };
+
+  name = "nextjs-app";
+  version = "0.1.0";
+}
--- modules/flake-parts/checks.nix
+++ modules/flake-parts/checks.nix
@@ -1,28 +1,53 @@
 {
   self,
   lib,
+  inputs,
   ...
 }: {
   perSystem = {
     self',
     pkgs,
+    system,
     ...
   }: let
+    dream2nixFlake = import ../../. {};
+
+    importFlake = flakeFile: let
+      self' = (import flakeFile).outputs {
+        dream2nix = dream2nixFlake;
+        nixpkgs = inputs.nixpkgs;
+        self = self';
+      };
+    in
+      self';
+
     modules = self.modules.dream2nix;
+
     modulesWithTests =
       lib.filterAttrs
       (_: module: lib.pathExists (module + /tests/packages))
       modules;
+
     getPackagePaths = moduleName: modulePath:
       lib.concatMapAttrs
       (packageDir: _: {
         "module-${moduleName}-${packageDir}" = "${modulePath}/tests/packages/${packageDir}";
       })
       (builtins.readDir (modulePath + /tests/packages));
-    makePackage = testModulePath: let
+
+    makePackage = path:
+      if lib.pathExists (path + "/flake.nix")
+      then makePackageFromFlake path
+      else makePackageFromDefaultNix path;
+
+    makePackageFromFlake = flakePath:
+      (importFlake (flakePath + "/flake.nix")).packages.${system}.default or {};
+
+    makePackageFromDefaultNix = testModulePath: let
       evaled = lib.evalModules {
         specialArgs = {
-          dream2nix.modules = self.modules;
+          dream2nix = dream2nixFlake;
+
           packageSets.nixpkgs = pkgs;
         };
         modules = [
@@ -35,10 +60,14 @@
       };
     in
       evaled.config.public;
+
     packagesToBuild =
       lib.concatMapAttrs getPackagePaths modulesWithTests;
+
     packagesBuilt = lib.mapAttrs (_: makePackage) packagesToBuild;
+
+    packagesFiltered = lib.filterAttrs (_: pkg: pkg != {}) packagesBuilt;
   in {
-    checks = self'.packages // packagesBuilt;
+    checks = self'.packages // packagesFiltered;
   };
 }
