32938533a1cb74ae8f253d71ad2ea62f87fcd3d3
DavHau <hsngrmpf+github@gmail.com>
2024-03-17 22:49:39 +0700

    mkDerivation/builtins-derivation: refactor: implementation.nix -> default.nix

--- modules/dream2nix/builtins-derivation/default.nix
+++ modules/dream2nix/builtins-derivation/default.nix
@@ -1,6 +1,50 @@
 {
+  config,
+  lib,
+  ...
+}: let
+  l = lib // builtins;
+  t = l.types;
+
+  cfg = config.builtins-derivation;
+
+  outputs = l.unique cfg.outputs;
+
+  keepArg = key: val: val != null;
+
+  finalArgs = l.filterAttrs keepArg cfg;
+
+  # ensure that none of the env variables collide with the top-level options
+  envChecked =
+    l.mapAttrs
+    (key: val:
+      if config.builtins-derivation.${key} or false
+      then throw (envCollisionError key)
+      else val)
+    config.env;
+
+  # generates error message for env variable collision
+  envCollisionError = key: ''
+    Error while evaluating definitions for derivation ${config.name}
+    The environment variable defined via `env.${key}' collides with the option builtins-derivation.`${key}'.
+    Specify the top-level option instead, or rename the environment variable.
+  '';
+in {
   imports = [
-    ./implementation.nix
     ./interface.nix
+    ../core
+    ../package-func
   ];
+
+  config.package-func.outputs = cfg.outputs;
+
+  config.package-func.func = lib.mkDefault builtins.derivation;
+
+  config.package-func.args =
+    envChecked
+    // finalArgs
+    // {
+      inherit outputs;
+      inherit (config.public) name;
+    };
 }
