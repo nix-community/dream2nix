e5ca903b65b547601f17c63939387f48ec293cc6
purepani <pani0028@umn.edu>
2024-03-27 17:55:57 -0500

    overrides/python: Adds qt support for opencv

--- overrides/python/opencv-python/default.nix
+++ overrides/python/opencv-python/default.nix
@@ -5,16 +5,23 @@
 }: {
   # TODO: supply more of the dependencies instead of ignoring them
   env.autoPatchelfIgnoreMissingDeps = true;
-  mkDerivation.buildInputs = [
-    config.deps.libglvnd
-    config.deps.glib
-  ];
+  mkDerivation = {
+    buildInputs = [
+      config.deps.libglvnd
+      config.deps.glib
+      config.deps.qt6Packages.qtbase
+    ];
+    nativeBuildInputs = [
+      config.deps.qt6Packages.wrapQtAppsHook
+    ];
+  };
   deps = {nixpkgs, ...}:
     lib.mapAttrs (_: lib.mkDefault) {
       inherit
         (nixpkgs)
         libglvnd
         glib
+        qt6Packages
         ;
     };
 }
