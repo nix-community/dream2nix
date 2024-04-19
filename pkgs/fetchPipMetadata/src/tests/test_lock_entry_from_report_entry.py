844625b04a0384ff5d1ca1644d3e7b67625f3e78
phaer <hello@phaer.org>
2024-04-19 15:40:14 +0200

    fetchPipMetadata: deduplicate test fixtures

--- pkgs/fetchPipMetadata/src/tests/test_lock_entry_from_report_entry.py
+++ pkgs/fetchPipMetadata/src/tests/test_lock_entry_from_report_entry.py
@@ -1,100 +1,73 @@
 from pathlib import Path
 from packaging.requirements import Requirement
-
 import lock_file_from_report as l
 
 
-def test_url_no_hash():
-    install = dict(
+def metadata(**kwargs):
+    return dict(
         metadata=dict(
             name="test",
             version="0.0.0",
         ),
-        download_info=dict(url="https://example.com"),
+        **kwargs,
     )
-    expected = "test", dict(
+
+
+def expect(install, sha256=None):
+    return "test", dict(
         type="url",
         url=install["download_info"]["url"],
         version=install["metadata"]["version"],
-        sha256=None,
+        sha256=sha256,
     )
+
+
+def test_url_no_hash():
+    install = metadata(
+        download_info=dict(url="https://example.com"),
+    )
+    expected = expect(install)
     assert l.lock_entry_from_report_entry(install, Path("foo")) == expected
 
 
 def test_url_with_hash():
-    install = dict(
-        metadata=dict(
-            name="test",
-            version="0.0.0",
-        ),
+    install = metadata(
         download_info=dict(
             url="https://example.com",
             archive_info=dict(hash=f"sha256=example_hash"),
         ),
     )
-    expected = "test", dict(
-        type="url",
-        url=install["download_info"]["url"],
-        version=install["metadata"]["version"],
-        sha256="example_hash",
-    )
+    expected = expect(install, sha256="example_hash")
     assert l.lock_entry_from_report_entry(install, Path("foo")) == expected
 
 
 def test_path_external():
-    install = dict(
-        metadata=dict(
-            name="test",
-            version="0.0.0",
-        ),
+    install = metadata(
         download_info=dict(
             url="/foo/bar",
         ),
     )
-    expected = "test", dict(
-        type="url",
-        url=install["download_info"]["url"],
-        version=install["metadata"]["version"],
-        sha256=None,
-    )
+    expected = expect(install)
     assert l.lock_entry_from_report_entry(install, Path("foo")) == expected
 
 
 def test_path_in_repo(git_repo_path):
-    install = dict(
-        metadata=dict(
-            name="test",
-            version="0.0.0",
-        ),
+    install = metadata(
         download_info=dict(
             url=git_repo_path.name,
         ),
     )
-    expected = "test", dict(
-        type="url",
-        url=install["download_info"]["url"],
-        version=install["metadata"]["version"],
-        sha256=None,
-    )
+    expected = expect(install)
     assert l.lock_entry_from_report_entry(install, Path("foo")) == expected
 
 
 def test_path_in_nix_store():
-    install = dict(
-        metadata=dict(
-            name="test",
-            version="0.0.0",
-        ),
+    install = metadata(
         download_info=dict(
             url="/nix/store/test",
         ),
     )
-    expected = "test", dict(
-        type="url",
-        url=install["download_info"]["url"],
-        version=install["metadata"]["version"],
-        sha256=None,
-    )
+    expected = expect(install)
     assert l.lock_entry_from_report_entry(install, Path("foo")) == expected
 
 
