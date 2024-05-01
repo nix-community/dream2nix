cec919bc01838546921911ddfbc37c29cf1eace9
phaer <hello@phaer.org>
2024-05-02 00:00:09 +0200

    editable: refactor into functions

--- modules/dream2nix/pip/editable.py
+++ modules/dream2nix/pip/editable.py
@@ -20,6 +20,100 @@ def run(args):
         sys.exit(1)
 
 
+def make_editable(site_dir, editables_dir, site_packages, drvs, name, path):
+    normalized_name = name.replace("-", "_")
+    editable_dir = editables_dir / name
+    drv_out = Path(drvs[name]["out"])
+    if editable_dir.exists():
+        print(f"Skipping existing editable source in {editable_dir}", file=sys.stderr)
+        return
+
+    # Build the non-editable package if it's not in /nix/store already.
+    # We need its .dist-info directory and might need it's unpackaged
+    # source below.
+    if not drv_out.exists():
+        run(["nix", "build", "--no-link", drv_out])
+
+    make_editable_source(editable_dir, normalized_name, path)
+    make_pth(site_dir, editable_dir, normalized_name)
+    make_dist_info(site_dir, editable_dir, site_packages, drv_out)
+
+
+def make_editable_source(editable_dir, normalized_name, path):
+    if path != None:
+        source = path
+    elif name == root_name:
+        source = root_dir
+    else:
+        source = drvs[name]["src"]
+
+    # Create a copy of the source in editable_dir
+    if str(source).startswith("/nix/store") and str(source).endswith(".whl"):
+        print(
+            f"Extracting editable source from {source} to {editable_dir}",
+            file=sys.stderr,
+        )
+        run(
+            [
+                f"{unzip}/bin/unzip",
+                "-q",
+                "-d",
+                str(editable_dir),
+                source,
+                f"{normalized_name}/*",
+            ]
+        )
+    elif str(source).startswith("/nix/store"):
+        print(
+            f"Copying editable source from {source} to {editable_dir}",
+            file=sys.stderr,
+        )
+        shutil.copytree(source, editable_dir, symlinks=True)
+        run(["chmod", "-R", "u+w", editable_dir])
+    else:
+        print(
+            f"Linking editable source from {source} to {editable_dir}",
+            file=sys.stderr,
+        )
+        run(["ln", "-sf", source, editable_dir])
+
+
+def make_pth(site_dir, editable_dir, normalized_name):
+    # Check if source uses a "src"-layout or a flat-layout and
+    # write the .pth file
+    if (editable_dir / "src").exists():
+        pth = editable_dir / "src"
+    else:
+        # TODO this approach is risky as it puts everything inside
+        # upstreams repo on $PYTHONPATH. Maybe we should try to
+        # get packages from toplevel.txt first and if found,
+        # create a dir with only them linked?
+        pth = editable_dir
+    with open((site_dir / normalized_name).with_suffix(".pth"), "w") as f:
+        f.write(f"{pth}\n")
+
+
+def make_dist_info(site_dir, editable_dir, site_packages, drv_out):
+    # Reuse dist_info from the non-editable derivation and make it editable
+    installed_dist_infos = list((drv_out / site_packages).glob("*.dist-info"))
+    assert len(installed_dist_infos) == 1
+    installed_dist_info = installed_dist_infos[0]
+    editable_dist_info = site_dir / installed_dist_info.name
+    shutil.copytree(installed_dist_info, editable_dist_info, symlinks=True)
+    run(["chmod", "-R", "u+w", editable_dist_info])
+
+    if (editable_dist_info / "RECORD").exists():
+        # PEP-660 says RECORD should not be included
+        (editable_dist_info / "RECORD").unlink()
+
+    with open(editable_dist_info / "direct_url.json", "w") as f:
+        json.dump(
+            {"url": f"file://{editable_dir}", "dir_info": {"editable": True}},
+            f,
+            indent=2,
+        )
+
+
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as f:
         args = json.load(f)
@@ -45,89 +139,7 @@ if __name__ == "__main__":
     run(["nix", "build", "--no-link", python_environment])
 
     for name, path in editables.items():
-        normalized_name = name.replace("-", "_")
-        editable_dir = editables_dir / name
-        drv_out = Path(drvs[name]["out"])
-        if editable_dir.exists():
-            print(
-                f"Skipping existing editable source in {editable_dir}", file=sys.stderr
-            )
-            continue
-
-        # Build the non-editable package if it's not in /nix/store already.
-        # We need its .dist-info directory and might need it's unpackaged
-        # source below.
-        if not drv_out.exists():
-            run(["nix", "build", "--no-link", drv_out])
-
-        if path != None:
-            source = path
-        elif name == root_name:
-            source = root_dir
-        else:
-            source = drvs[name]["src"]
-
-        # Create a copy of the source in editable_dir
-        if str(source).startswith("/nix/store") and str(source).endswith(".whl"):
-            print(
-                f"Extracting editable source from {source} to {editable_dir}",
-                file=sys.stderr,
-            )
-            run(
-                [
-                    f"{unzip}/bin/unzip",
-                    "-q",
-                    "-d",
-                    str(editable_dir),
-                    source,
-                    f"{normalized_name}/*",
-                ]
-            )
-        elif str(source).startswith("/nix/store"):
-            print(
-                f"Copying editable source from {source} to {editable_dir}",
-                file=sys.stderr,
-            )
-            shutil.copytree(source, editable_dir, symlinks=True)
-            run(["chmod", "-R", "u+w", editable_dir])
-        else:
-            print(
-                f"Linking editable source from {source} to {editable_dir}",
-                file=sys.stderr,
-            )
-            run(["ln", "-sf", source, editable_dir])
-
-        # Check if source uses a "src"-layout or a flat-layout and
-        # write the .pth file
-        if (editable_dir / "src").exists():
-            pth = editable_dir / "src"
-        else:
-            # TODO this approach is risky as it puts everything inside
-            # upstreams repo on $PYTHONPATH. Maybe we should try to
-            # get packages from toplevel.txt first and if found,
-            # create a dir with only them linked?
-            pth = editable_dir
-        with open((site_dir / normalized_name).with_suffix(".pth"), "w") as f:
-            f.write(f"{pth}\n")
-
-        # Reuse dist_info from the non-editable derivation and make it editable
-        installed_dist_infos = list((drv_out / site_packages).glob("*.dist-info"))
-        assert len(installed_dist_infos) == 1
-        installed_dist_info = installed_dist_infos[0]
-        editable_dist_info = site_dir / installed_dist_info.name
-        shutil.copytree(installed_dist_info, editable_dist_info, symlinks=True)
-        run(["chmod", "-R", "u+w", editable_dist_info])
-
-        if (editable_dist_info / "RECORD").exists():
-            # PEP-660 says RECORD should not be included
-            (editable_dist_info / "RECORD").unlink()
-
-        with open(editable_dist_info / "direct_url.json", "w") as f:
-            json.dump(
-                {"url": f"file://{editable_dir}", "dir_info": {"editable": True}},
-                f,
-                indent=2,
-            )
+        make_editable(site_dir, editables_dir, site_packages, drvs, name, path)
 
     with open(site_dir / "sitecustomize.py", "w") as f:
         f.write(
