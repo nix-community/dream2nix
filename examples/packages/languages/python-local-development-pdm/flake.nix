42282329b09ba4bac2f17d3b834652607002ecd1
phaer <hello@phaer.org>
2024-06-28 18:29:28 +0200

    tree-wide: make examples eval on all platforms...
    all 4 default platforms of nixpkgs that is.

--- examples/packages/languages/python-local-development-pdm/flake.nix
+++ examples/packages/languages/python-local-development-pdm/flake.nix
@@ -1,39 +1,60 @@
 {
-  description = "My flake with dream2nix packages";
+  # This example flake.nix is pretty generic and the same for all
+  # examples, except when they define devShells or extra packages.
+  description = "Dream2nix example flake";
 
+  # We import the latest commit of dream2nix main branch and instruct nix to
+  # re-use the nixpkgs revision referenced by dream2nix.
+  # This is what we test in CI with, but you can generally refer to any
+  # recent nixpkgs commit here.
   inputs = {
     dream2nix.url = "github:nix-community/dream2nix";
     nixpkgs.follows = "dream2nix/nixpkgs";
   };
 
-  outputs = inputs @ {
+  outputs = {
     self,
     dream2nix,
     nixpkgs,
-    ...
   }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    # A helper that helps us define the attributes below for
+    # all systems we care about.
+    eachSystem = nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
   in {
-    packages.${system}.default = dream2nix.lib.evalModules {
-      packageSets.nixpkgs = pkgs;
-      modules = [
-        ./default.nix
-        {
-          paths.projectRoot = ./.;
-          # can be changed to ".git" or "flake.nix" to get rid of .project-root
-          paths.projectRootFile = "flake.nix";
-          paths.package = ./.;
-        }
-      ];
-    };
-    devShells.${system}.default = pkgs.mkShell {
-      # inherit from the dream2nix generated dev shell
-      inputsFrom = [self.packages.${system}.default.devShell];
-      # add extra packages
-      packages = [
-        pkgs.hello
-      ];
-    };
+    packages = eachSystem (system: {
+      # For each system, we define our default package
+      # by passing in our desired nixpkgs revision plus
+      # any dream2nix modules needed by it.
+      default = dream2nix.lib.evalModules {
+        packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
+        modules = [
+          # Import our actual package definiton as a dream2nix module from ./default.nix
+          ./default.nix
+          {
+            # Aid dream2nix to find the project root. This setup should also works for mono
+            # repos. If you only have a single project, the defaults should be good enough.
+            paths.projectRoot = ./.;
+            # can be changed to ".git" or "flake.nix" to get rid of .project-root
+            paths.projectRootFile = "flake.nix";
+            paths.package = ./.;
+          }
+        ];
+      };
+    });
+    devShells = eachSystem (system: {
+      default = nixpkgs.legacyPackages.${system}.mkShell {
+        # inherit from the dream2nix generated dev shell
+        inputsFrom = [self.packages.${system}.default.devShell];
+        # add extra packages
+        packages = [
+          nixpkgs.legacyPackages.${system}.hello
+        ];
+      };
+    });
   };
 }
