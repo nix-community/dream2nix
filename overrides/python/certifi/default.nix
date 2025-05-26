b3e7c37bfd36283fbe4720319bac57df3fb46c4c
h0nIg <h0nIg@users.noreply.github.com>
2025-05-26 11:11:53 +0200

    override: python certifi

---
+++ overrides/python/certifi/default.nix
@@ -0,0 +1,13 @@
+{
+  config,
+  lib,
+  dream2nix,
+  ...
+}: let
+  isSdist = lib.hasSuffix ".tar.gz" config.mkDerivation.src;
+in {
+  # enable overrides from nixpkgs
+  imports = [dream2nix.modules.dream2nix.nixpkgs-overrides];
+  nixpkgs-overrides.enable = isSdist;
+  nixpkgs-overrides.exclude = ["propagatedBuildInputs"];
+}
