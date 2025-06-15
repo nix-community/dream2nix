d74c70cbd94f8c8a2efa3b20a510c5d3e55cc3e7
DavHau <hsngrmpf+github@gmail.com>
2025-06-15 17:19:55 +0700

    tests/python-torch: fix test for linux and darwin

--- examples/packages/languages/python-local-development-pdm-pytorch-from-nixpkgs/flake.nix
+++ examples/packages/languages/python-local-development-pdm-pytorch-from-nixpkgs/flake.nix
@@ -20,7 +20,9 @@
     # A helper that helps us define the attributes below for
     # all systems we care about.
     eachSystem = nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
       "aarch64-linux"
+      "x86_64-darwin"
       "x86_64-linux"
     ];
   in {
